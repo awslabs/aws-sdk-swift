a601b3db91ac242bee327f275ddbce65df35646a
Nicki Stone <nickik@amazon.com>
2020-08-31 16:18:55 -0700

    moving code out to proper place to account for swift variability

--- codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/RestJsonProtocolGenerator.kt
+++ codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/RestJsonProtocolGenerator.kt
@@ -24,6 +24,8 @@ import software.amazon.smithy.model.shapes.*
 import software.amazon.smithy.model.traits.JsonNameTrait
 import software.amazon.smithy.model.traits.TimestampFormatTrait
 import software.amazon.smithy.swift.codegen.ServiceGenerator
+import software.amazon.smithy.swift.codegen.SwiftWriter
+import software.amazon.smithy.swift.codegen.defaultName
 import software.amazon.smithy.swift.codegen.integration.HttpBindingProtocolGenerator
 import software.amazon.smithy.swift.codegen.integration.HttpFeature
 import software.amazon.smithy.swift.codegen.integration.HttpRequestEncoder
@@ -37,44 +39,41 @@ import software.amazon.smithy.swift.codegen.integration.ProtocolGenerator
 abstract class RestJsonProtocolGenerator : HttpBindingProtocolGenerator() {
     override val defaultTimestampFormat: TimestampFormatTrait.Format = TimestampFormatTrait.Format.EPOCH_SECONDS
 
-    override fun generateProtocolUnitTests(ctx: ProtocolGenerator.GenerationContext) {
-        val ignoredTests = setOf(
-                "RestJsonListsSerializeNull", // TODO - sparse lists not supported - this test needs removed
-                "RestJsonSerializesNullMapValues", // TODO - sparse maps not supported - this test needs removed
-                // FIXME - document type not fully supported yet
-                "InlineDocumentInput",
-                "InlineDocumentAsPayloadInput",
-                "InlineDocumentOutput",
-                "InlineDocumentAsPayloadInputOutput"
-        )
+//    override fun generateProtocolUnitTests(ctx: ProtocolGenerator.GenerationContext) {
+//        val ignoredTests = setOf(
+//                "RestJsonListsSerializeNull", // TODO - sparse lists not supported - this test needs removed
+//                "RestJsonSerializesNullMapValues", // TODO - sparse maps not supported - this test needs removed
+//                // FIXME - document type not fully supported yet
+//                "InlineDocumentInput",
+//                "InlineDocumentAsPayloadInput",
+//                "InlineDocumentOutput",
+//                "InlineDocumentAsPayloadInputOutput"
+//        )
+//
+//        val requestTestBuilder = HttpProtocolUnitTestRequestGenerator.Builder()
+//
+//        // TODO:: add response generator too
+//        HttpProtocolTestGenerator(
+//                ctx,
+//                requestTestBuilder,
+//                ignoredTests
+//        ).generateProtocolTests()
+//    }
 
-        val requestTestBuilder = HttpProtocolUnitTestRequestGenerator.Builder()
-
-        // TODO:: add response generator too
-        HttpProtocolTestGenerator(
-                ctx,
-                requestTestBuilder,
-                ignoredTests
-        ).generateProtocolTests()
-    }
-
-    override fun generateSerializers(ctx: ProtocolGenerator.GenerationContext) {
-        // Generate extension on input requests to implement Encodable protocol
-        val inputShapesWithHttpBindings:MutableSet<ShapeId> = mutableSetOf()
-        for (operation in getHttpBindingOperations(ctx)) {
-            if (operation.input.isPresent) {
-                val inputShapeId = operation.input.get()
-                if (inputShapesWithHttpBindings.contains(inputShapeId)) {
-                    // The input shape is referenced by more than one operation
-                    continue
+    override fun generateCodingKeysForStructure(ctx: ProtocolGenerator.GenerationContext, writer: SwiftWriter, shape: StructureShape) {
+        val membersSortedByName: List<MemberShape> = shape.allMembers.values.sortedBy { ctx.symbolProvider.toMemberName(it) }
+        writer.openBlock("private enum CodingKeys: String, CodingKey {", "}") {
+            for (member in membersSortedByName) {
+                val memberName = ctx.symbolProvider.toMemberName(member)
+                if (member.hasTrait(JsonNameTrait::class.java)) {
+                    val jsonName = member.getTrait(JsonNameTrait::class.java).get().value
+                    writer.write("case $memberName = $jsonName")
+                } else {
+                    writer.write("case $memberName")
                 }
-                renderInputRequestConformanceToEncodable(ctx, operation)
-                inputShapesWithHttpBindings.add(inputShapeId)
             }
         }
-        // This only considers the structures referenced from any input request structure for any operation
-        renderConformanceToCodableForReferencedStructures(ctx)
-        super.generateSerializers(ctx)
+        writer.write("")
     }
 
     override fun getHttpFeatures(ctx: ProtocolGenerator.GenerationContext): List<HttpFeature> {
@@ -86,84 +85,6 @@ abstract class RestJsonProtocolGenerator : HttpBindingProtocolGenerator() {
         return features
     }
 
-    private fun getDateEncodingStrategy(timestampFormat: TimestampFormatTrait.Format): String {
-        return when (timestampFormat) {
-            TimestampFormatTrait.Format.DATE_TIME -> ".formatted(DateFormatter.iso8601DateFormatterWithFractionalSeconds)"
-            TimestampFormatTrait.Format.HTTP_DATE -> ".formatted(DateFormatter.rfc5322DateFormatter)"
-            // TODO:: handle this case
-            TimestampFormatTrait.Format.EPOCH_SECONDS -> ""
-            else -> ""
-        }
-    }
-
-    private fun renderInputRequestConformanceToEncodable(ctx: ProtocolGenerator.GenerationContext, op: OperationShape) {
-        if (op.input.isEmpty()) {
-            return
-        }
-        val inputShape = ctx.model.expectShape(op.input.get())
-        val opIndex = ctx.model.getKnowledge(OperationIndex::class.java)
-        val inputShapeName = ServiceGenerator.getOperationInputShapeName(ctx.symbolProvider, opIndex, op)
-        val requestPayloadMemberCodingKeys = getCodingKeysForPayloadMembers(ctx, op)
-        val rootNamespace = ctx.settings.moduleName
-        val encodeSymbol = Symbol.builder()
-            .definitionFile("./${rootNamespace}/models/${inputShapeName}+Encodable.swift")
-            .name(inputShapeName)
-            .build()
-        ctx.delegator.useShapeWriter(encodeSymbol) { writer ->
-            writer.openBlock("extension ${inputShapeName}: Encodable {", "}") {
-                if (requestPayloadMemberCodingKeys.isNotEmpty()) {
-                    writer.openBlock("private enum CodingKeys: String, CodingKey {", "}") {
-                        writer.write("case ${requestPayloadMemberCodingKeys.joinToString(separator = ", ")}")
-                    }
-                }
-            }
-            writer.write("")
-        }
-    }
-
-    private fun renderConformanceToCodableForReferencedStructures(ctx: ProtocolGenerator.GenerationContext) {
-        val structuresNeedingCodableConformance = resolveStructuresNeedingCodableConformance(ctx)
-        for (structureShape in structuresNeedingCodableConformance) {
-            // conforming to Codable and Coding Keys enum are rendered as separate extensions
-            renderDefaultConformanceToProtocolForStructure(ctx, structureShape, "Encodable")
-            renderCodingKeysForStructure(ctx, structureShape)
-        }
-    }
-
-    // render empty conformance to Codable for Structure as an Extension
-    private fun renderDefaultConformanceToProtocolForStructure(ctx: ProtocolGenerator.GenerationContext, structureShape: Shape, protocolName: String) {
-        val structSymbol: Symbol = ctx.symbolProvider.toSymbol(structureShape)
-        ctx.delegator.useShapeWriter(structureShape) { writer ->
-            writer.write("extension ${structSymbol.name}: $protocolName {}")
-            writer.write("")
-        }
-    }
-
-    private fun renderCodingKeysForStructure(ctx: ProtocolGenerator.GenerationContext, structureShape: StructureShape) {
-        val structSymbol: Symbol = ctx.symbolProvider.toSymbol(structureShape)
-        val membersSortedByName: List<MemberShape> = structureShape.allMembers.values.sortedBy { ctx.symbolProvider.toMemberName(it) }
-        val hasJSONNameTrait = membersSortedByName.any { it.hasTrait(JsonNameTrait::class.java) }
-        if (hasJSONNameTrait) {
-            val codingKeysEnumCases = mutableListOf<String>()
-            for (member in membersSortedByName) {
-                val memberName = ctx.symbolProvider.toMemberName(member)
-                if (member.hasTrait(JsonNameTrait::class.java)) {
-                    codingKeysEnumCases.add(constructEnumCaseForMemberWithJSONName(ctx, member))
-                } else {
-                    codingKeysEnumCases.add(memberName)
-                }
-            }
-
-            ctx.delegator.useShapeWriter(structureShape) { writer ->
-                writer.openBlock("extension ${structSymbol.name} {", "}") {
-                    writer.openBlock("private enum CodingKeys: String, CodingKey {", "}") {
-                        writer.write("case ${codingKeysEnumCases.joinToString(separator = ", ")}")
-                    }
-                }
-                writer.write("")
-            }
-        }
-    }
 
     private fun constructEnumCaseForMemberWithJSONName(ctx: ProtocolGenerator.GenerationContext, memberShape: MemberShape): String {
         val memberName = ctx.symbolProvider.toMemberName(memberShape)
@@ -205,48 +126,7 @@ abstract class RestJsonProtocolGenerator : HttpBindingProtocolGenerator() {
         return requestPayloadMemberCodingKeys
     }
 
-    /**
-     * Find and return the set of shapes that are not operation inputs but need `Codable` conformance.
-     * Operation inputs conform to `Encodable`, everything else conforms to `Codable`.
-     *
-     * @return The set of shapes that require a `Codable` conformance and possibly custom coding keys.
-     */
-    private fun resolveStructuresNeedingCodableConformance(ctx: ProtocolGenerator.GenerationContext): Set<StructureShape> {
-        // all top level operation inputs conform to Encodable
-        // any structure shape that shows up as a nested member (direct or indirect) conforms to Codable
-        val topLevelMembers = getHttpBindingOperations(ctx)
-                .filter { it.input.isPresent }
-                .flatMap {
-                    val inputShape = ctx.model.expectShape(it.input.get())
-                    inputShape. members()
-                }
-                .map { ctx.model.expectShape(it.target) }
-                .filter { it.isStructureShape || it is CollectionShape || it.isMapShape }
-                .toSet()
 
-        val resolved = mutableSetOf<StructureShape>()
-        val walker = Walker(ctx.model)
-
-        // walk all operation inputs that are of type struct and find all other
-        // structs in the graph from that shape
-        topLevelMembers.forEach { shape ->
-            walker.iterateShapes(shape) { relationship ->
-                when (relationship.relationshipType) {
-                    RelationshipType.MEMBER_TARGET,
-                    RelationshipType.STRUCTURE_MEMBER,
-                    RelationshipType.LIST_MEMBER,
-                    RelationshipType.SET_MEMBER,
-                    RelationshipType.MAP_VALUE -> true
-                    else -> false
-                }
-            }.forEach { walkedShape ->
-                if (walkedShape.type == ShapeType.STRUCTURE) {
-                    resolved.add(walkedShape as StructureShape)
-                }
-            }
-        }
-        return resolved
-    }
 }
 
 
