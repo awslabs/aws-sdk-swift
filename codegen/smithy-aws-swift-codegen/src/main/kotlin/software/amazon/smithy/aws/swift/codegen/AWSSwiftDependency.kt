ec67bd9f43f19f3a3cd55b62f045c0f69112e7a2
John Woo <wooj@amazon.com>
2021-01-20 22:18:41 -0800

    fix: path generation for local dev

--- codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/AWSSwiftDependency.kt
+++ codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/AWSSwiftDependency.kt
@@ -5,9 +5,14 @@
 package software.amazon.smithy.aws.swift.codegen
 import software.amazon.smithy.codegen.core.SymbolDependency
 import software.amazon.smithy.codegen.core.SymbolDependencyContainer
+import java.io.File
 
 enum class AWSSwiftDependency(val type: String, val namespace: String, val version: String, val url: String, var packageName: String) : SymbolDependencyContainer {
-    AWS_CLIENT_RUNTIME("", "AWSClientRuntime", "0.1.0", "~/Projects/Amplify/SwiftSDK/aws-sdk-swift/AWSClientRuntime", "AWSClientRuntime");
+    AWS_CLIENT_RUNTIME("",
+        "AWSClientRuntime",
+        "0.1.0",
+        computeAbsolutePath("aws-sdk-swift/AWSClientRuntime"),
+        "AWSClientRuntime");
 
     override fun getDependencies(): List<SymbolDependency> {
         val dependency = SymbolDependency.builder()
@@ -19,4 +24,17 @@ enum class AWSSwiftDependency(val type: String, val namespace: String, val versi
                 .build()
         return listOf(dependency)
     }
+}
+
+private fun computeAbsolutePath(relativePath: String): String {
+    var userDirPath = System.getProperty("user.dir")
+    while (userDirPath.isNotEmpty()) {
+        val fileName = userDirPath.removeSuffix("/") + "/" + relativePath
+        val file = File(fileName)
+        if (file.isDirectory) {
+            return fileName
+        }
+        userDirPath = userDirPath.substring(0, userDirPath.length-1)
+    }
+    return ""
 }
