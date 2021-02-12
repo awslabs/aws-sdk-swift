c7c0c62a4d7452b6ef8368b0c0bc33718b6ab71b
John Woo <wooj@amazon.com>
2021-02-12 15:33:30 -0800

    updating unit tests

--- codegen/smithy-aws-swift-codegen/src/test/kotlin/software/amazon/smithy/aws/swift/codegen/AwsJson1_1ProtocolGeneratorTests.kt
+++ codegen/smithy-aws-swift-codegen/src/test/kotlin/software/amazon/smithy/aws/swift/codegen/AwsJson1_1ProtocolGeneratorTests.kt
@@ -912,8 +912,7 @@ class KitchenSinkOperationRequestTest: HttpRequestTestBase {
         deserializeStackStep: mockDeserializeStackStep)
         _ = operationStack.handleMiddleware(context: context, input: input, next: MockHandler(){ (context, request) in
             XCTFail("Deserialize was mocked out, this should fail")
-            return .failure(try! MockMiddlewareError(httpResponse: HttpResponse()))
-
+            return .failure(try! MockMiddlewareError(httpResponse: HttpResponse(body: .none, statusCode: .badRequest)))
         })
         wait(for: [deserializeMiddleware], timeout: 0.3)
     }
@@ -987,8 +986,7 @@ class KitchenSinkOperationRequestTest: HttpRequestTestBase {
         deserializeStackStep: mockDeserializeStackStep)
         _ = operationStack.handleMiddleware(context: context, input: input, next: MockHandler(){ (context, request) in
             XCTFail("Deserialize was mocked out, this should fail")
-            return .failure(try! MockMiddlewareError(httpResponse: HttpResponse()))
-
+            return .failure(try! MockMiddlewareError(httpResponse: HttpResponse(body: .none, statusCode: .badRequest)))
         })
         wait(for: [deserializeMiddleware], timeout: 0.3)
     }
