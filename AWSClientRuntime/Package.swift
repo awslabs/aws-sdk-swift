43c3c1d726f8cf333a6b00db27ec506d0d666b0a
Nicki Stone <nickik@amazon.com>
2021-02-19 15:57:44 -0800

    add inner pacakge.swift with header file template for development purposes

---
+++ AWSClientRuntime/Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "AWSClientRuntime",
+    platforms: [
+	.macOS(.v10_15),
+	.iOS(.v13)
+    ],
+    products: [
+        .library(name: "AWSClientRuntime", targets: ["AWSClientRuntime"])
+    ],
+    dependencies: [
+        .package(name: "ClientRuntime", url: "https://github.com/awslabs/smithy-swift", .branch("fix/ci")),
+        .package(name: "AwsCrt", url: "https://github.com/awslabs/aws-crt-swift", .branch("master"))
+    ],
+    targets: [
+        .target(
+            name: "AWSClientRuntime",
+            dependencies: [
+                .product(name: "ClientRuntime", package: "ClientRuntime"),
+                .product(name: "AwsCommonRuntimeKit", package: "AwsCrt")
+            ],
+            path: "./Sources"
+        ),
+        .testTarget(
+            name: "AWSClientRuntimeTests",
+            dependencies: [
+                "AWSClientRuntime",
+                .product(name: "SmithyTestUtil", package: "ClientRuntime"),
+                .product(name: "ClientRuntime", package: "ClientRuntime")
+            ],
+            path: "./Tests"
+        )
+    ]
+)
--- codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/AWSSwiftDependency.kt
+++ codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/AWSSwiftDependency.kt
@@ -5,7 +5,6 @@
 package software.amazon.smithy.aws.swift.codegen
 import software.amazon.smithy.codegen.core.SymbolDependency
 import software.amazon.smithy.codegen.core.SymbolDependencyContainer
-import java.io.File
 
 enum class AWSSwiftDependency(val type: String, val target: String, val branch: String?, val version: String, val url: String, var packageName: String) : SymbolDependencyContainer {
     AWS_CLIENT_RUNTIME(
@@ -39,9 +38,8 @@ private fun getGitBranchName(): String {
         sb.append(char.toChar())
     }
     var branchName = sb.removeSuffix("\n").toString()
-    if(branchName == "HEAD") {
+    if (branchName == "HEAD") {
         branchName = System.getenv("BRANCH_NAME")
     }
     return branchName
 }
-
