be84fcebf63eb986d991dbc27eeec77b4898b27c
Nicki Stone <nickik@amazon.com>
2021-03-17 19:03:56 -0700

    chore: fix to swift branch dependency mgmt when not local or ci (#66)

--- codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/AWSSwiftDependency.kt
+++ codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/AWSSwiftDependency.kt
@@ -7,24 +7,31 @@ import software.amazon.smithy.codegen.core.SymbolDependency
 import software.amazon.smithy.codegen.core.SymbolDependencyContainer
 import software.amazon.smithy.swift.codegen.resources.Resources
 
-enum class AWSSwiftDependency(val type: String, val target: String, val branch: String?, val version: String, val url: String, var packageName: String) : SymbolDependencyContainer {
+enum class AWSSwiftDependency(
+    val target: String,
+    private val branch: String?,
+    private val version: String,
+    private val url: String,
+    private val localPath: String,
+    var packageName: String
+) : SymbolDependencyContainer {
     AWS_CLIENT_RUNTIME(
-        "",
         "AWSClientRuntime",
-        null,
+        "master",
         "0.1.0",
+        "https://github.com/awslabs/aws-sdk-swift",
         Resources.computeAbsolutePath("aws-sdk-swift/AWSClientRuntime", "AWS_SDK_SWIFT_CI_DIR"),
         "AWSClientRuntime"
     );
 
     override fun getDependencies(): List<SymbolDependency> {
         val dependency = SymbolDependency.builder()
-            .dependencyType(type)
             .putProperty("target", target)
             .packageName(packageName)
             .version(version)
             .putProperty("url", url)
             .putProperty("branch", branch)
+            .putProperty("localPath", localPath)
             .build()
         return listOf(dependency)
     }
