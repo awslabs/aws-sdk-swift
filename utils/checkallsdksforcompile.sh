a9fefe0b2a8d19f9d4d26c666f4132fe1b6fc928
Nicki Stone <nickik@amazon.com>
2021-03-22 21:32:05 -0700

    removing script from pr

--- utils/checkallsdksforcompile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p ~/sdkBuildOutput
-cd ../codegen/sdk-codegen/build/smithyprojections/sdk-codegen
-for dir in `ls |grep -v "source"`; do
-    pushd $dir/swift-codegen/
-    swift build > ~/sdkBuildOutput/$dir.txt
-    pwd
-    popd
-done
