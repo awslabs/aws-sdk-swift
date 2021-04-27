61dc57a6d7f26695676bff081956e3c5ea7f0a11
John Woo <wooj@amazon.com>
2021-04-27 13:31:47 -0700

    update per pr

--- codegen/smithy-aws-swift-codegen/src/test/kotlin/software/amazon/smithy/aws/swift/codegen/mocks/MockRestJson1BindingProtocolGenerator.kt
+++ codegen/smithy-aws-swift-codegen/src/test/kotlin/software/amazon/smithy/aws/swift/codegen/mocks/MockRestJson1BindingProtocolGenerator.kt
@@ -3,7 +3,7 @@ package software.amazon.smithy.aws.swift.codegen.mocks
 import software.amazon.smithy.aws.swift.codegen.AWSHttpBindingProtocolGenerator
 import software.amazon.smithy.aws.swift.codegen.AWSHttpProtocolClientCustomizableFactory
 import software.amazon.smithy.aws.swift.codegen.AWSHttpProtocolClientGeneratorFactory
-import software.amazon.smithy.aws.swift.codegen.awsjson.AWSHttpResponseBindingAWSJson
+import software.amazon.smithy.aws.swift.codegen.restjson.AWSHttpResponseBindingRestJson
 import software.amazon.smithy.aws.traits.protocols.RestJson1Trait
 import software.amazon.smithy.model.shapes.ShapeId
 import software.amazon.smithy.model.traits.TimestampFormatTrait
@@ -16,7 +16,7 @@ class MockRestJson1BindingProtocolGenerator : AWSHttpBindingProtocolGenerator() 
     override val httpProtocolClientGeneratorFactory = AWSHttpProtocolClientGeneratorFactory()
     override val httpProtocolCustomizable = AWSHttpProtocolClientCustomizableFactory().constructClientCustomizable(protocol)
     override val httpResponseGenerator = HttpResponseGenerator(
-        AWSHttpResponseBindingAWSJson(),
+        AWSHttpResponseBindingRestJson(),
         serviceErrorProtocolSymbol,
         unknownServiceErrorSymbol,
         defaultTimestampFormat
