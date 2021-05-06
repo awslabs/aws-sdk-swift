33df1fc27c81678456de4dfc04ac0e06c512fa16
Nicki Stone <nickik@amazon.com>
2021-05-05 21:30:15 -0700

    revert john patch and upgrade smithy gradle plugin to eliminate loc header issue (#85)

--- codegen/sdk-codegen/build.gradle.kts
+++ codegen/sdk-codegen/build.gradle.kts
@@ -15,7 +15,7 @@ import kotlin.streams.toList
 import java.util.Properties
 
 plugins {
-    id("software.amazon.smithy") version "0.5.2"
+    id("software.amazon.smithy") version "0.5.3"
 }
 
 dependencies {
