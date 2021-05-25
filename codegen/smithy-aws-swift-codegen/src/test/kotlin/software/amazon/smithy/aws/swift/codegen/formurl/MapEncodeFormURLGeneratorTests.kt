e485979c357daf428b1b662e63f086ffb836907e
John Woo <wooj@amazon.com>
2021-05-25 15:03:23 -0700

    in progress of formurl

--- codegen/smithy-aws-swift-codegen/src/test/kotlin/software/amazon/smithy/aws/swift/codegen/formurl/MapEncodeFormURLGeneratorTests.kt
+++ codegen/smithy-aws-swift-codegen/src/test/kotlin/software/amazon/smithy/aws/swift/codegen/formurl/MapEncodeFormURLGeneratorTests.kt
@@ -4,33 +4,20 @@ import io.kotest.matchers.string.shouldContainOnlyOnce
 import org.junit.jupiter.api.Test
 import software.amazon.smithy.aws.swift.codegen.TestContext
 import software.amazon.smithy.aws.swift.codegen.TestContextGenerator
-import software.amazon.smithy.aws.swift.codegen.TestContextGenerator.Companion.listFilesFromManifest
 import software.amazon.smithy.aws.swift.codegen.awsquery.AwsQueryProtocolGenerator
 import software.amazon.smithy.aws.swift.codegen.shouldSyntacticSanityCheck
 import software.amazon.smithy.aws.traits.protocols.AwsQueryTrait
 
-class StructEncodeFormURLGeneratorTests {
+class MapEncodeFormURLGeneratorTests {
     @Test
-    fun `001 hardcodes action and version into input type`() {
-        val context = setupTests("awsquery/query-idempotency-token.smithy", "aws.protocoltests.query#AwsQuery")
-        val contents = TestContextGenerator.getFileContents(context.manifest, "/Example/models/QueryIdempotencyTokenAutoFillInput+Encodable.swift")
+    fun `001 encode different types of maps`() {
+        val context = setupTests("awsquery/query-maps.smithy", "aws.protocoltests.query#AwsQuery")
+        print(TestContextGenerator.listFilesFromManifest(context.manifest))
+        val contents = TestContextGenerator.getFileContents(context.manifest, "/Example/models/QueryMapsInput+Encodable.swift")
         contents.shouldSyntacticSanityCheck()
         val expectedContents =
             """
-            extension QueryIdempotencyTokenAutoFillInput: Encodable, Reflection {
-                enum CodingKeys: String, CodingKey {
-                    case token
-                }
-            
-                public func encode(to encoder: Encoder) throws {
-                    var container = encoder.container(keyedBy: Key.self)
-                    if let token = token {
-                        try container.encode(token, forKey: Key("token"))
-                    }
-                    try container.encode("QueryIdempotencyTokenAutoFill", forKey:Key("Action"))
-                    try container.encode("2020-01-08", forKey:Key("Version"))
-                }
-            }
+
             """.trimIndent()
         contents.shouldContainOnlyOnce(expectedContents)
     }
--- codegen/smithy-aws-swift-codegen/src/test/resources/software.amazon.smithy.aws.swift.codegen/awsquery/query-idempotency-token.smithy
+++ codegen/smithy-aws-swift-codegen/src/test/resources/software.amazon.smithy.aws.swift.codegen/awsquery/query-idempotency-token.smithy
@@ -1,4 +1,3 @@
-
 $version: "1.0"
 
 namespace aws.protocoltests.query
