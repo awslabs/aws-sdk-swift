76ee19c2f19e666c68a46be43c3c4167433e2da1
John Woo <wooj@amazon.com>
2021-05-27 10:24:08 -0700

    chore: extend body middleware to be pluggable per protocol

--- codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/awsquery/AwsQueryProtocolGenerator.kt
+++ codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/awsquery/AwsQueryProtocolGenerator.kt
@@ -5,11 +5,15 @@ import software.amazon.smithy.aws.swift.codegen.AWSHttpProtocolClientCustomizabl
 import software.amazon.smithy.aws.swift.codegen.AWSHttpProtocolClientGeneratorFactory
 import software.amazon.smithy.aws.swift.codegen.awsjson.AWSJsonHttpResponseBindingErrorGenerator
 import software.amazon.smithy.aws.traits.protocols.AwsQueryTrait
+import software.amazon.smithy.codegen.core.Symbol
 import software.amazon.smithy.model.shapes.MemberShape
 import software.amazon.smithy.model.shapes.Shape
 import software.amazon.smithy.model.shapes.ShapeId
 import software.amazon.smithy.model.traits.TimestampFormatTrait
+import software.amazon.smithy.swift.codegen.Middleware
 import software.amazon.smithy.swift.codegen.SwiftWriter
+import software.amazon.smithy.swift.codegen.integration.HttpBindingDescriptor
+import software.amazon.smithy.swift.codegen.integration.HttpBodyMiddleware
 import software.amazon.smithy.swift.codegen.integration.ProtocolGenerator
 import software.amazon.smithy.swift.codegen.integration.httpResponse.HttpResponseGenerator
 import software.amazon.smithy.swift.codegen.model.ShapeMetadata
@@ -42,5 +46,18 @@ open class AwsQueryProtocolGenerator : AWSHttpBindingProtocolGenerator() {
         defaultTimestampFormat: TimestampFormatTrait.Format,
     ) {
         // TODO: Fill in implementation
+    }
+
+    override fun httpBodyMiddleware(
+        writer: SwiftWriter,
+        ctx: ProtocolGenerator.GenerationContext,
+        inputSymbol: Symbol,
+        outputSymbol: Symbol,
+        outputErrorSymbol: Symbol,
+        requestBindings: List<HttpBindingDescriptor>
+    ): Middleware {
+        // TODO: Create new implementation of HttpBodyMiddleware which ignores HTTP binding traits per:
+        //      https://awslabs.github.io/smithy/1.0/spec/aws/aws-query-protocol.html#protocol-behavior
+        return HttpBodyMiddleware(writer, ctx, inputSymbol, outputSymbol, outputErrorSymbol, requestBindings)
     }
 }
