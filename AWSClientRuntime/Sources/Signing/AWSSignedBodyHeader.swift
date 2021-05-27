88b4f9aa2457c8df1106c79c53f36296786eac0e
Nicki Stone <nickik@amazon.com>
2021-05-26 18:14:57 -0700

    remove documentation per pr comments

--- AWSClientRuntime/Sources/Signing/AWSSignedBodyHeader.swift
+++ AWSClientRuntime/Sources/Signing/AWSSignedBodyHeader.swift
@@ -8,10 +8,7 @@
 import AwsCommonRuntimeKit
 
 public enum AWSSignedBodyHeader {
-    /// Do not add a header
     case none
-
-    /// Add the "x-amz-content-sha256" header with the canonical request's body value
     case contentSha256
 }
 
--- AWSClientRuntime/Sources/Signing/SigningFlags.swift
+++ AWSClientRuntime/Sources/Signing/SigningFlags.swift
@@ -7,17 +7,8 @@
 import AwsCommonRuntimeKit
         
 public struct SigningFlags {
-    /// We assume the uri will be encoded once in preparation for transmission.  Certain services
-    /// do not decode before checking signature, requiring us to actually double-encode the uri in the canonical
-    /// request in order to pass a signature check.
     let useDoubleURIEncode: Bool
-
-    /// Controls whether or not the uri paths should be normalized when building the canonical request
     let shouldNormalizeURIPath: Bool
-
-    /// Should the "X-Amz-Security-Token" query param be omitted?
-    /// Normally, this parameter is added during signing if the credentials have a session token.
-    /// The only known case where this should be true is when signing a websocket handshake to IoT Core.
     let omitSessionToken: Bool
 }
 
