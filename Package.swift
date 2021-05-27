eab4d4271247d2eef95d300310498b0632efd328
John Woo <wooj@amazon.com>
2021-05-27 12:02:55 -0700

    chore: update package.swift file

--- Package.swift
+++ Package.swift
@@ -3,13 +3,19 @@ import PackageDescription
 import class Foundation.ProcessInfo
 
 let package = Package(
-    name: "AWSClientRuntime",
+    name: "AWSSwiftSDK",
     platforms: [
-	.macOS(.v10_15),
-	.iOS(.v13)
+        .macOS(.v10_15),
+        .iOS(.v13)
     ],
     products: [
-        .library(name: "AWSClientRuntime", targets: ["AWSClientRuntime"])
+        .library(name: "AWSClientRuntime", targets: ["AWSClientRuntime"]),
+        .library(name: "Lambda", targets: ["Lambda"]),
+        .library(name: "Dynamodb", targets: ["Dynamodb"]),
+        .library(name: "Cognitoidentityprovider", targets: ["Cognitoidentityprovider"]),
+        .library(name: "Cognitoidentity", targets: ["Cognitoidentity"]),
+        .library(name: "S3", targets: ["S3"]),
+        .library(name: "Pinpoint", targets: ["Pinpoint"]),
     ],
     targets: [
         .target(
@@ -19,6 +25,71 @@ let package = Package(
                 .product(name: "AwsCommonRuntimeKit", package: "AwsCrt")
             ],
             path: "./AWSClientRuntime/Sources"
+        ),
+        .target(
+            name: "Lambda",
+            dependencies: [
+                .product(
+                    name: "ClientRuntime",
+                    package: "ClientRuntime"
+                ),
+                "AWSClientRuntime"
+            ],
+            path: "./AlphaSwiftSDK/Lambda"
+        ),
+        .target(
+            name: "Dynamodb",
+            dependencies: [
+                .product(
+                    name: "ClientRuntime",
+                    package: "ClientRuntime"
+                ),
+                "AWSClientRuntime"
+            ],
+            path: "./AlphaSwiftSDK/Dynamodb"
+        ),
+        .target(
+            name: "Cognitoidentityprovider",
+            dependencies: [
+                .product(
+                    name: "ClientRuntime",
+                    package: "ClientRuntime"
+                ),
+                "AWSClientRuntime"
+            ],
+            path: "./AlphaSwiftSDK/Cognitoidentityprovider"
+        ),
+        .target(
+            name: "Cognitoidentity",
+            dependencies: [
+                .product(
+                    name: "ClientRuntime",
+                    package: "ClientRuntime"
+                ),
+                "AWSClientRuntime"
+            ],
+            path: "./AlphaSwiftSDK/Cognitoidentity"
+        ),
+        .target(
+            name: "S3",
+            dependencies: [
+                .product(
+                    name: "ClientRuntime",
+                    package: "ClientRuntime"
+                ),
+                "AWSClientRuntime"
+            ],
+            path: "./AlphaSwiftSDK/S3"
+        ),
+        .target(name: "Pinpoint",
+            dependencies: [
+                .product(
+                    name: "ClientRuntime",
+                    package: "ClientRuntime"
+                ),
+                "AWSClientRuntime"
+            ],
+            path: "./AlphaSwiftSDK/Pinpoint"
         ),
         .testTarget(
             name: "AWSClientRuntimeTests",
@@ -34,14 +105,7 @@ let package = Package(
 
 let relatedDependenciesBranch = "master"
 
-if ProcessInfo.processInfo.environment["SWIFTSDK_DEPS_USE_LOCAL_PATHS"] == nil {
-    package.dependencies += [
-        .package(name: "AwsCrt", url: "https://github.com/awslabs/aws-crt-swift", .branch(relatedDependenciesBranch)),
-        .package(name: "ClientRuntime", url: "https://github.com/awslabs/smithy-swift", .branch(relatedDependenciesBranch))
-    ]
-} else {
-    package.dependencies += [
-        .package(name: "AwsCrt", url: "https://github.com/awslabs/aws-crt-swift", .branch(relatedDependenciesBranch)),
-        .package(name: "ClientRuntime", path: "./target/build/deps/smithy-swift")
-    ]
-}
+package.dependencies += [
+    .package(name: "AwsCrt", url: "https://github.com/awslabs/aws-crt-swift", .branch(relatedDependenciesBranch)),
+    .package(name: "ClientRuntime", url: "https://github.com/awslabs/smithy-swift", .branch(relatedDependenciesBranch))
+]
