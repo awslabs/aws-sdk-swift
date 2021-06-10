02eae659df43b029c9f9ad07164534cc031096c4
John Woo <wooj@amazon.com>
2021-06-10 11:17:36 -0700

    fix: restxml error parsing

--- AWSClientRuntime/Tests/Protocols/RestXML/Models/Errors/GreetingWithErrorsOutputError+ResponseInit.swift
+++ AWSClientRuntime/Tests/Protocols/RestXML/Models/Errors/GreetingWithErrorsOutputError+ResponseInit.swift
@@ -8,19 +8,20 @@
 import AWSClientRuntime
 import ClientRuntime
 
+extension GreetingWithErrorsOutputError: HttpResponseBinding {
+    public init(httpResponse: HttpResponse, decoder: ResponseDecoder? = nil) throws {
+        let errorDetails = try RestXMLError(httpResponse: httpResponse)
+        try self.init(errorType: errorDetails.errorCode, httpResponse: httpResponse, decoder: decoder, message: errorDetails.message, requestID: errorDetails.requestId)
+    }
+}
+
 extension GreetingWithErrorsOutputError {
     public init(errorType: String?, httpResponse: HttpResponse, decoder: ResponseDecoder? = nil, message: String? = nil, requestID: String? = nil) throws {
         switch errorType {
         case "ComplexXMLError" : self = .complexXMLError(try ComplexXMLError(httpResponse: httpResponse, decoder: decoder, message: message, requestID: requestID))
         case "InvalidGreeting" : self = .invalidGreeting(try InvalidGreeting(httpResponse: httpResponse, decoder: decoder, message: message, requestID: requestID))
-        default : self = .unknown(UnknownAWSHttpServiceError(httpResponse: httpResponse, message: message))
+        default : self = .unknown(UnknownAWSHttpServiceError(httpResponse: httpResponse, message: message, requestID: requestID))
         }
     }
 }
 
-extension GreetingWithErrorsOutputError: HttpResponseBinding {
-    public init(httpResponse: HttpResponse, decoder: ResponseDecoder? = nil) throws {
-        let errorDetails = try RestXMLError(httpResponse: httpResponse)
-        try self.init(errorType: errorDetails.errorCode, httpResponse: httpResponse, decoder: decoder, message: nil, requestID: errorDetails.requestId)
-    }
-}
