5e7f26fb2561027f8536f43c2630e2a5b2835761
John Woo <wooj@amazon.com>
2021-06-30 17:36:22 -0700

    updating package.swift to target 1.0.0 and update readme

--- Package.swift
+++ Package.swift
@@ -278,8 +278,8 @@ let package = Package(
         .library(name: "XRay", targets: ["XRay"]),
     ],
     dependencies: [
-        .package(name: "AwsCrt", url: "https://github.com/awslabs/aws-crt-swift", .branch("master")),
-        .package(name: "ClientRuntime", url: "https://github.com/awslabs/smithy-swift", .branch("master"))
+        .package(name: "AwsCrt", url: "https://github.com/awslabs/aws-crt-swift.git", from: "1.0.0"),
+        .package(name: "ClientRuntime", url: "https://github.com/awslabs/smithy-swift.git", from: "1.0.0")
     ],
     targets: [
         .target(
