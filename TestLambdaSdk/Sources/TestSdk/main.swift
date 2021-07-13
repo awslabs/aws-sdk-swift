21f8c77ab3d157e96093691a323531875f634b0b
Nicki Stone <nickik@amazon.com>
2021-07-13 11:43:00 -0700

    changes to s3 test

--- TestLambdaSdk/Sources/TestSdk/main.swift
+++ TestLambdaSdk/Sources/TestSdk/main.swift
@@ -1,15 +1,16 @@
 import S3
 import Foundation
 import ClientRuntime
+import AwsCommonRuntimeKit
 
 let config = try! S3Client.S3ClientConfiguration.default()
 let s3Client = S3Client(config: config)
 let data = "hello".data(using: .utf8)
-let input = PutObjectInput(body: data, bucket: "nickijustinsite", key: "index.html")
+let input = PutObjectInput(body: ByteStream.fromData(data: data!),
+                           bucket: "nickijustinsite", key: "index.html")
 
-let fileProvider = StreamSourceProvider.fromData(data: data!)
 
-s3Client.putObject(input: input, streamSource: fileProvider.unwrap()) { completion in
+s3Client.putObject(input: input) { completion in
     switch completion {
     case .failure(let error):
         print(error)
