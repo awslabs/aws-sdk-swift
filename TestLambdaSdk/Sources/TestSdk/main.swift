129893ba78fc9f8c4013e71216ad4c3b10f3fa17
Nicki Stone <nickik@amazon.com>
2021-07-19 16:22:47 -0700

    fix: streaming (#165)

--- TestLambdaSdk/Sources/TestSdk/main.swift
+++ TestLambdaSdk/Sources/TestSdk/main.swift
@@ -2,20 +2,27 @@ import S3
 import Foundation
 import ClientRuntime
 
-let config = try! S3Client.S3ClientConfiguration.default()
-let s3Client = S3Client(config: config)
-let data = "hello".data(using: .utf8)
-let input = PutObjectInput(body: data, bucket: "nickijustinsite", key: "index.html")
-
-let fileProvider = StreamSourceProvider.fromData(data: data!)
-
-s3Client.putObject(input: input, streamSource: fileProvider.unwrap()) { completion in
-    switch completion {
-    case .failure(let error):
-        print(error)
-    case .success(let object):
-        print("success")
-        print(object)
+do {
+    
+    let config = try S3Client.S3ClientConfiguration.default()
+    let s3Client = S3Client(config: config)
+    guard let data = "hello".data(using: .utf8) else {
+        print("why is data empty")
+        fatalError()
     }
+    let input = PutObjectInput(body: ByteStream.from(data: data),
+                               bucket: "nickijustinsite", key: "index.html")
+    
+    s3Client.putObject(input: input) { completion in
+        switch completion {
+        case .failure(let error):
+            print(error)
+        case .success(let object):
+            print("success")
+            print(object)
+        }
+    }
+    
+} catch let err {
+    print(err)
 }
-
