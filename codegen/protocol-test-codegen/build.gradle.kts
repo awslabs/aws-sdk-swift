77992a7175df6d56a0b01e7e0b227db026507ed6
John Woo <1911939+wooj2@users.noreply.github.com>
2021-08-02 13:29:30 -0700

    chore: generate smithy-build.json for protocol test codegen (#285)

--- codegen/protocol-test-codegen/build.gradle.kts
+++ codegen/protocol-test-codegen/build.gradle.kts
@@ -25,6 +25,20 @@ dependencies {
     implementation(project(":smithy-aws-swift-codegen"))
 }
 
+val enabledProtocols = listOf(
+    ProtocolTest("ec2-query", "aws.protocoltests.ec2#AwsEc2", "Ec2QueryTestSDK"),
+    ProtocolTest("aws-json-10", "aws.protocoltests.json10#JsonRpc10", "AWSJson1_0TestSDK"),
+    ProtocolTest("aws-json-11", "aws.protocoltests.json#JsonProtocol", "AWSJson1_1TestSDK"),
+    ProtocolTest("aws-restjson", "aws.protocoltests.restjson#RestJson", "AWSRestJsonTestSDK"),
+    ProtocolTest("rest-xml", "aws.protocoltests.restxml#RestXml", "RestXmlTestSDK"),
+    //ProtocolTest("rest-xml-xmlns", "aws.protocoltests.restxml.xmlns#RestXmlWithNamespace", "RestXmlWithNamespaceTestSDK"),
+    ProtocolTest("aws-query", "aws.protocoltests.query#AwsQuery", "AWSQueryTestSDK")
+
+    // service specific tests
+    //ProtocolTest("apigateway", "com.amazonaws.apigateway#BackplaneControlService"),
+    //ProtocolTest("glacier", "com.amazonaws.glacier#Glacier")
+)
+
 // This project doesn't produce a JAR.
 tasks["jar"].enabled = false
 
@@ -32,8 +46,24 @@ tasks["jar"].enabled = false
 // from smithy-aws-swift-codegen.
 tasks["smithyBuildJar"].enabled = false
 
+task("generateSmithyBuild") {
+    group = "codegen"
+    description = "generate smithy-build.json"
+    val buildFile = projectDir.resolve("smithy-build.json")
+    doFirst {
+        buildFile.writeText(generateSmithyBuild(enabledProtocols))
+    }
+    outputs.file(buildFile)
+}
+
+tasks["clean"].doFirst {
+    delete("smithy-build.json")
+}
+
 tasks.create<SmithyBuild>("buildSdk") {
     addRuntimeClasspath = true
+    dependsOn(tasks["generateSmithyBuild"])
+    inputs.file(projectDir.resolve("smithy-build.json"))
 }
 
 // Run the `buildSdk` automatically.
@@ -42,17 +72,58 @@ tasks["build"].finalizedBy(tasks["buildSdk"])
 // force rebuild every time while developing
 tasks["buildSdk"].outputs.upToDateWhen { false }
 
-// The following section exposes Smithy protocol test suites as gradle test targets
-// for the configured protocols in [enabledProtocols].
-val enabledProtocols = listOf("aws-json-10", "aws-json-11", "aws-restjson", "rest-xml", "aws-query", "ec2-query")
-
 enabledProtocols.forEach {
-    tasks.register<ProtocolTestTask>("testProtocol-${it}") {
+    tasks.register<ProtocolTestTask>("testProtocol-${it.projectionName}") {
         dependsOn(tasks.build)
         group = "Verification"
-        protocol = it
+        protocol = it.projectionName
         plugin = "swift-codegen"
     }
+}
+
+
+data class ProtocolTest(val projectionName: String,
+                        val serviceShapeId: String,
+                        val moduleName: String) {
+    val packageName: String
+        get() = projectionName.toLowerCase().filter { it.isLetterOrDigit() }
+}
+fun generateSmithyBuild(tests: List<ProtocolTest>): String {
+    val projections = tests.joinToString(",") { test ->
+        """
+            "${test.projectionName}": {
+              "transforms": [
+                {
+                  "name": "includeServices",
+                  "args": {
+                    "services": [
+                      "${test.serviceShapeId}"
+                    ]
+                  }
+                }
+              ],
+              "plugins": {
+                "swift-codegen": {
+                  "service": "${test.serviceShapeId}",
+                  "module": "${test.moduleName}",
+                  "moduleVersion": "1.0",
+                  "gitRepo": "https://github.com/aws-amplify/smithy-swift.git",
+                  "author": "Amazon Web Services",
+                  "homepage": "https://docs.amplify.aws/",
+                  "swiftVersion": "5.4.0",
+                  "shouldGenerateUnitTestTarget": true
+                }
+              }
+            }"""
+    }
+    return """
+    {
+        "version": "1.0",
+        "projections": {
+            $projections
+        }
+    }
+    """.trimIndent()
 }
 
 tasks.register("testAllProtocols") {
