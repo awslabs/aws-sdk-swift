be318ee2c60f7815cfec1518ef62194b088e99cb
John Woo <wooj@amazon.com>
2021-08-19 11:42:28 -0700

    fix: error type of modeled service errors

--- codegen/smithy-aws-swift-codegen/src/test/kotlin/software/amazon/smithy/aws/swift/codegen/restxml/AWSRestXMLHttpResponseBindingErrorGeneratorTests.kt
+++ codegen/smithy-aws-swift-codegen/src/test/kotlin/software/amazon/smithy/aws/swift/codegen/restxml/AWSRestXMLHttpResponseBindingErrorGeneratorTests.kt
@@ -90,9 +90,41 @@ class AWSRestXMLHttpResponseBindingErrorGeneratorTests {
             """.trimIndent()
         contents.shouldContainOnlyOnce(expectedContents)
     }
-
     @Test
-    fun `004 ComplexXMLErrorNoErrorWrapping Init renders without container`() {
+    fun `004 ComplexXMLError extends from AWSHttpServiceError (soon to be changed to S3 Specific) `() {
+        val context = setupTests("restxml/xml-errors.smithy", "aws.protocoltests.restxml#RestXml")
+        val contents = getFileContents(context.manifest, "/Example/models/ComplexXMLError.swift")
+        contents.shouldSyntacticSanityCheck()
+        val expectedContents =
+            """            
+            public struct ComplexXMLError: AWSClientRuntime.AWSHttpServiceError, Swift.Equatable {
+                public var _headers: ClientRuntime.Headers?
+                public var _statusCode: ClientRuntime.HttpStatusCode?
+                public var _message: Swift.String?
+                public var _requestID: Swift.String?
+                public var _retryable: Swift.Bool = false
+                public var _isThrottling: Swift.Bool = false
+                public var _type: ClientRuntime.ErrorType = .client
+                public var header: Swift.String?
+                public var nested: RestXmlClientTypes.ComplexXMLNestedErrorData?
+                public var topLevel: Swift.String?
+            
+                public init (
+                    header: Swift.String? = nil,
+                    nested: RestXmlClientTypes.ComplexXMLNestedErrorData? = nil,
+                    topLevel: Swift.String? = nil
+                )
+                {
+                    self.header = header
+                    self.nested = nested
+                    self.topLevel = topLevel
+                }
+            }
+            """.trimIndent()
+        contents.shouldContainOnlyOnce(expectedContents)
+    }
+    @Test
+    fun `005 ComplexXMLErrorNoErrorWrapping Init renders without container`() {
         val context = setupTests("restxml/xml-errors-noerrorwrapping.smithy", "aws.protocoltests.restxml#RestXml")
         val contents = getFileContents(context.manifest, "/Example/models/ComplexXMLErrorNoErrorWrapping+Init.swift")
         contents.shouldSyntacticSanityCheck()
