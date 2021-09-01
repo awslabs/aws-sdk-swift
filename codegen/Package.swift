9d2422b38fe2062dcd0cea3b6ec35f1d2bf94559
John Woo <wooj@amazon.com>
2021-09-01 13:46:06 -0700

    chore: enable api gateway & glacier tests

--- codegen/Package.swift
+++ codegen/Package.swift
@@ -39,6 +39,10 @@ appendLibTarget(name: "AWSQueryTestSDK", path: "\(baseDir)/aws-query")
 appendTstTarget(name: "AWSQueryTestSDKTests", path: "\(baseDir)/aws-query", dependency: "AWSQueryTestSDK")
 
 //Service specific
+appendLibTarget(name: "GlacierTestSDK", path: "\(baseDir)/glacier")
+appendTstTarget(name: "GlacierTestSDKTests", path: "\(baseDir)/glacier", dependency: "GlacierTestSDK")
+appendLibTarget(name: "APIGatewayTestSDK", path: "\(baseDir)/apigateway")
+appendTstTarget(name: "APIGatewayTestSDKTests", path: "\(baseDir)/apigateway", dependency: "APIGatewayTestSDK")
 //appendLibTarget(name: "S3TestSDK", path: "\(baseDir)/s3")
 //appendTstTarget(name: "S3TestSDKTests", path: "\(baseDir)/s3", dependency: "S3TestSDK")
 
--- codegen/protocol-test-codegen/build.gradle.kts
+++ codegen/protocol-test-codegen/build.gradle.kts
@@ -32,11 +32,11 @@ val enabledProtocols = listOf(
     ProtocolTest("aws-restjson", "aws.protocoltests.restjson#RestJson", "AWSRestJsonTestSDK"),
     ProtocolTest("rest-xml", "aws.protocoltests.restxml#RestXml", "RestXmlTestSDK"),
     ProtocolTest("rest-xml-xmlns", "aws.protocoltests.restxml.xmlns#RestXmlWithNamespace", "RestXmlWithNamespaceTestSDK"),
-    ProtocolTest("aws-query", "aws.protocoltests.query#AwsQuery", "AWSQueryTestSDK")
+    ProtocolTest("aws-query", "aws.protocoltests.query#AwsQuery", "AWSQueryTestSDK"),
 
     // service specific tests
-    //ProtocolTest("apigateway", "com.amazonaws.apigateway#BackplaneControlService"),
-    //ProtocolTest("glacier", "com.amazonaws.glacier#Glacier", "GlacierTestSDK")
+    ProtocolTest("apigateway", "com.amazonaws.apigateway#BackplaneControlService", "APIGatewayTestSDK"),
+    ProtocolTest("glacier", "com.amazonaws.glacier#Glacier", "GlacierTestSDK")
     //ProtocolTest("s3", "com.amazonaws.s3#AmazonS3", "S3TestSDK")
 )
 
