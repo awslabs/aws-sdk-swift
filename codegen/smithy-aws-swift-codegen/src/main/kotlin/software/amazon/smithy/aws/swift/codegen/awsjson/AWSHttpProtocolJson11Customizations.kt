e22c0fda7c7aa1057eabcbee34f3e2cf01579a3a
John Woo <1911939+wooj2@users.noreply.github.com>
2021-09-20 11:52:44 -0700

    chore: Remove middleware function from HttpProtocolCustomizable (#398)

--- codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/awsjson/AWSHttpProtocolJson11Customizations.kt
+++ codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/awsjson/AWSHttpProtocolJson11Customizations.kt
@@ -5,28 +5,6 @@
 
 package software.amazon.smithy.aws.swift.codegen.awsjson
 
-import software.amazon.smithy.aws.swift.codegen.AWSClientRuntimeTypes
 import software.amazon.smithy.aws.swift.codegen.AWSHttpProtocolCustomizations
-import software.amazon.smithy.aws.swift.codegen.AWSSwiftDependency
-import software.amazon.smithy.codegen.core.Symbol
-import software.amazon.smithy.protocoltests.traits.HttpRequestTestCase
-import software.amazon.smithy.swift.codegen.SwiftWriter
 
-class AWSHttpProtocolJson11Customizations : AWSHttpProtocolCustomizations() {
-
-    override fun renderMiddlewareForGeneratedRequestTests(
-        writer: SwiftWriter,
-        test: HttpRequestTestCase,
-        operationStack: String,
-        inputSymbol: Symbol,
-        outputSymbol: Symbol,
-        outputErrorName: String,
-        hasHttpBody: Boolean
-    ) {
-        writer.addImport(AWSSwiftDependency.AWS_CLIENT_RUNTIME.target)
-        if (test.headers.keys.contains("X-Amz-Target")) {
-            val XAmzTargetValue = test.headers["X-Amz-Target"]
-            writer.write("$operationStack.serializeStep.intercept(position: .before, middleware: \$N<${inputSymbol.name}, $outputSymbol, $outputErrorName>(xAmzTarget: \"${XAmzTargetValue}\"))", AWSClientRuntimeTypes.AWSJSON.XAmzTargetMiddleware)
-        }
-    }
-}
+class AWSHttpProtocolJson11Customizations : AWSHttpProtocolCustomizations()
