30ccae53f2a8449b432d9b0214d1a252ae48838b
John Woo <1911939+wooj2@users.noreply.github.com>
2021-09-27 17:29:45 -0700

    feat: Implement GET presigner for synthesizespeech (#407)

---
+++ codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/customization/polly/PollySynthesizeSpeechGETQueryItemMiddleware.kt
@@ -0,0 +1,62 @@
+package software.amazon.smithy.aws.swift.codegen.customization.polly
+
+import software.amazon.smithy.codegen.core.Symbol
+import software.amazon.smithy.model.shapes.ListShape
+import software.amazon.smithy.model.shapes.Shape
+import software.amazon.smithy.model.shapes.ShapeId
+import software.amazon.smithy.swift.codegen.ClientRuntimeTypes.Core.URLQueryItem
+import software.amazon.smithy.swift.codegen.Middleware
+import software.amazon.smithy.swift.codegen.SwiftTypes
+import software.amazon.smithy.swift.codegen.SwiftWriter
+import software.amazon.smithy.swift.codegen.integration.ProtocolGenerator
+import software.amazon.smithy.swift.codegen.integration.steps.OperationSerializeStep
+import software.amazon.smithy.swift.codegen.model.isEnum
+
+class PollySynthesizeSpeechGETQueryItemMiddleware(
+    private val ctx: ProtocolGenerator.GenerationContext,
+    inputSymbol: Symbol,
+    outputSymbol: Symbol,
+    outputErrorSymbol: Symbol,
+    val inputShape: Shape,
+    private val writer: SwiftWriter
+) : Middleware(writer, inputSymbol, OperationSerializeStep(inputSymbol, outputSymbol, outputErrorSymbol)) {
+    override val typeName = "${inputSymbol.name}GETQueryItemMiddleware"
+
+    override fun generateMiddlewareClosure() {
+        for (member in inputShape.members()) {
+            val memberName = ctx.symbolProvider.toMemberName(member)
+            val queryKey = member.memberName
+
+            writer.openBlock("if let $memberName = input.operationInput.$memberName {", "}") {
+                val memberTargetShape = ctx.model.expectShape(member.target)
+                when (memberTargetShape) {
+                    is ListShape -> {
+                        val rawValueIfNeeded = rawValueIfNeeded(memberTargetShape.member.target)
+                        val queryValue = "item$rawValueIfNeeded"
+                        writer.openBlock("$memberName.forEach { item in ", "}") {
+                            writeRenderItem(queryKey, queryValue)
+                        }
+                    }
+                    else -> {
+                        val rawValueIfNeeded = rawValueIfNeeded(member.target)
+                        val queryValue = "${memberName}$rawValueIfNeeded"
+                        writeRenderItem(queryKey, queryValue)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun rawValueIfNeeded(shapeId: ShapeId): String {
+        return if (ctx.model.expectShape(shapeId).isEnum) ".rawValue" else ""
+    }
+
+    private fun writeRenderItem(queryKey: String, queryValue: String) {
+        writer.write("let queryItem = \$N(name: \"${queryKey}\".urlPercentEncoding(), value: \$N($queryValue).urlPercentEncoding())", URLQueryItem, SwiftTypes.String)
+        writer.write("input.builder.withQueryItem(queryItem)")
+    }
+
+    override fun generateInit() {
+        writer.write("public init() {}")
+    }
+}
