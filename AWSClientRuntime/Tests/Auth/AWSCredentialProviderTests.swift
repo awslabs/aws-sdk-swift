94219ca70f61a2e73851888c02129f7abfe17344
Nicki Stone <nickik@amazon.com>
2021-09-30 14:48:16 -0700

    remove new tests

--- AWSClientRuntime/Tests/Auth/AWSCredentialProviderTests.swift
+++ AWSClientRuntime/Tests/Auth/AWSCredentialProviderTests.swift
@@ -19,58 +19,12 @@ class AWSCredentialProviderTests: XCTestCase {
         XCTAssertEqual(credentials.accessKey, "MYACCESSKEY")
         XCTAssertEqual(credentials.secret, "sekrit")
     }
-    
-    func testYouCanUseCustomCredentialsProviderFromDiffThread() throws {
-        let awsCredsProvider = try AWSCredentialsProvider.fromCustom(MyCustomThreadTestCredentialsProvider())
-        let credentialsResult = try awsCredsProvider.getCredentials()
-        let credentials = try credentialsResult.get()
-        XCTAssertEqual(credentials.accessKey, "MYACCESSKEY")
-        XCTAssertEqual(credentials.secret, "sekrit")
-    }
-    
-    func testCredentialsProviderIsNonBlocking() throws {
-        let awsCredsProvider = try AWSCredentialsProvider.fromCustom(MyCustomThreadTestCredentialsProvider())
-        let expectation = XCTestExpectation(description: "credentials received")
-        DispatchQueue.global().async {
-            do {
-                let credentialsResult = try awsCredsProvider.getCredentials()
-                let credentials = try credentialsResult.get()
-                XCTAssertEqual(credentials.accessKey, "MYACCESSKEY")
-                XCTAssertEqual(credentials.secret, "sekrit")
-                expectation.fulfill()
-            } catch let err {
-                XCTFail(err.localizedDescription)
-            }
-        }
-        let thisCodeShouldStillRun = true
-        XCTAssertTrue(thisCodeShouldStillRun)
-        wait(for: [expectation], timeout: 20.0)
-    }
 }
 
 struct MyCustomCredentialsProvider: CredentialsProvider {
     func getCredentials() throws -> SdkFuture<AWSCredentials> {
         let future = SdkFuture<AWSCredentials>()
         future.fulfill(AWSCredentials(accessKey: "MYACCESSKEY", secret: "sekrit", expirationTimeout: 30))
-        return future
-    }
-}
-
-struct MyCustomThreadTestCredentialsProvider: CredentialsProvider {
-    func getCredentials() throws -> SdkFuture<AWSCredentials> {
-        let group = DispatchGroup()
-        let future = SdkFuture<AWSCredentials>()
-        for _ in 0...1000 {
-            group.enter()
-            DispatchQueue.global().async {
-                usleep(useconds_t(1000))
-                future.fulfill(AWSCredentials(accessKey: "MYACCESSKEY", secret: "sekrit", expirationTimeout: 30))
-                group.leave()
-            }
-        }
-        
-        let result = group.wait(timeout: DispatchTime.now() + 10)
-        XCTAssert(result == .success)
         return future
     }
 }
