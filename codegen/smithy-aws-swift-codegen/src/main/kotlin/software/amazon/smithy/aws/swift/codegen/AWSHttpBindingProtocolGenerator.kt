fa7bcde56c177ee48a00349851c71301201eb00c
Nicki Stone <nickik@amazon.com>
2021-10-07 15:37:18 -0700

    ignore tests as they are broken

--- codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/AWSHttpBindingProtocolGenerator.kt
+++ codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/AWSHttpBindingProtocolGenerator.kt
@@ -44,6 +44,11 @@ abstract class AWSHttpBindingProtocolGenerator : HttpBindingProtocolGenerator() 
     override val shouldRenderCodingKeysForEncodable = true
 
     override fun generateProtocolUnitTests(ctx: ProtocolGenerator.GenerationContext): Int {
+        //TODO: enable these tests once this PR is merged/released into Smithy: https://github.com/awslabs/smithy/pull/930
+        val ignoredTests = setOf(
+            "GlacierChecksums", // aws-sdk-swift#208
+            "GlacierMultipartChecksums", // aws-sdk-swift#208
+        )
         val imports = listOf(AWSSwiftDependency.AWS_CLIENT_RUNTIME.target)
         return HttpProtocolTestGenerator(
             ctx,
@@ -53,7 +58,8 @@ abstract class AWSHttpBindingProtocolGenerator : HttpBindingProtocolGenerator() 
             httpProtocolCustomizable,
             operationMiddleware,
             serdeContext,
-            imports
+            imports,
+            ignoredTests
         ).generateProtocolTests()
     }
 
