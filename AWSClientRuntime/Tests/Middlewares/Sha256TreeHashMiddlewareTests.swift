feb7c59bb2f14fbc0098678a29f0dbf95bbf922a
Nicki Stone <nickik@amazon.com>
2021-10-08 12:45:54 -0700

    adjust tests

--- AWSClientRuntime/Tests/Middlewares/Sha256TreeHashMiddlewareTests.swift
+++ AWSClientRuntime/Tests/Middlewares/Sha256TreeHashMiddlewareTests.swift
@@ -8,86 +8,44 @@
 import XCTest
 import ClientRuntime
 import AwsCommonRuntimeKit
+import SmithyTestUtil
 @testable import AWSClientRuntime
 
 class Sha256TreeHashMiddlewareTests: XCTestCase {
     override func setUp() {
+        #if os(Linux)
         AwsCommonRuntimeKit.initialize()
+        #endif
     }
     
     override func tearDown() {
+        #if os(Linux)
         AwsCommonRuntimeKit.cleanUp()
+        #endif
     }
     
-    func testTreeHashAllZeroes() {
+    func testTreeHashAllZeroes() throws {
         let context = HttpContextBuilder().build()
         let expectation = XCTestExpectation(description: "closure was run")
         let bytesIn5_5MB: Int = Int(1024 * 1024 * 5.5)
         let byteArray: [UInt8] = Array(repeating: 0, count: bytesIn5_5MB)
         let byteBuffer = ByteBuffer(bytes: byteArray)
-        let streamInput = StreamInput(body: ByteStream.buffer(byteBuffer))
-        var stack = OperationStack<StreamInput, StreamOutput, StreamError>(id: "TreeHashMiddlewareTestStack")
-        stack.serializeStep.intercept(position: .before, middleware: MockBodyMiddleware())
+        let streamInput = MockStreamInput(body: ByteStream.buffer(byteBuffer))
+        var stack = OperationStack<MockStreamInput, MockOutput, MockMiddlewareError>(id: "TreeHashMiddlewareTestStack")
+        stack.serializeStep.intercept(position: .before, middleware: MockSerializeStreamMiddleware())
+        let mockHttpResponse = HttpResponse(body: HttpBody.none, statusCode: .accepted)
+        let mockOutput = try MockOutput(httpResponse: mockHttpResponse, decoder: nil)
+        let output = OperationOutput<MockOutput>(httpResponse: mockHttpResponse, output: mockOutput)
         stack.finalizeStep.intercept(position: .after, middleware: Sha256TreeHashMiddleware())
-        _ = stack.handleMiddleware(context: context, input: streamInput, next: MockHandler(callback: { context, input in
+        _ = stack.handleMiddleware(context: context, input: streamInput, next: MockHandler(handleCallback: { context, input in
             let linear = input.headers.value(for: "X-Amz-Content-Sha256")
             XCTAssertEqual(linear, "733cf513448ce6b20ad1bc5e50eb27c06aefae0c320713a5dd99f4e51bc1ca60")
             let treeHash = input.headers.value(for: "X-Amz-Sha256-Tree-Hash")
             XCTAssertEqual(treeHash, "861890b487038d840e9d71d43bbc0fd4571453fb9d9b1f370caa3582a29b0ec7")
             expectation.fulfill()
+            return .success(output)
         }))
         
         wait(for: [expectation], timeout: 3.0)
     }
 }
-
-struct StreamInput: Reflection, Encodable {
-    let body: ByteStream
-}
-
-struct StreamOutput: Decodable, HttpResponseBinding {
-    init(httpResponse: HttpResponse, decoder: ResponseDecoder?) throws {}
-    init() {}
-}
-struct StreamError: HttpResponseBinding {
-    init(httpResponse: HttpResponse, decoder: ResponseDecoder?) throws {}
-}
-
-struct MockHandler: Handler {
-    let callback: (HttpContext, SdkHttpRequest) -> Void
-    func handle(context: HttpContext, input: SdkHttpRequest) -> Result<OperationOutput<StreamOutput>, SdkError<StreamError>> {
-        callback(context, input)
-        let streamOutput = StreamOutput()
-        return .success(OperationOutput(httpResponse: HttpResponse(body: .none, statusCode: .accepted),
-                                        output: streamOutput))
-    }
-    
-    typealias Input = SdkHttpRequest
-    
-    typealias Output = OperationOutput<StreamOutput>
-    
-    typealias Context = HttpContext
-    
-    typealias MiddlewareError = SdkError<StreamError>
-}
-
-struct MockBodyMiddleware: Middleware {
-    func handle<H>(context: HttpContext, input: SerializeStepInput<StreamInput>, next: H) -> Result<OperationOutput<StreamOutput>, SdkError<StreamError>> where H : Handler, HttpContext == H.Context, SdkError<StreamError> == H.MiddlewareError, SerializeStepInput<StreamInput> == H.Input, OperationOutput<StreamOutput> == H.Output {
-        input.builder.withBody(HttpBody.stream(input.operationInput.body))
-        return next.handle(context: context, input: input)
-    }
-    
-    var id: String = "MockBodyMiddleware"
-    
-    
-    typealias MInput = SerializeStepInput<StreamInput>
-    
-    typealias MOutput = OperationOutput<StreamOutput>
-    
-    typealias Context = HttpContext
-    
-    typealias MError = SdkError<StreamError>
-    
-    
-}
-
