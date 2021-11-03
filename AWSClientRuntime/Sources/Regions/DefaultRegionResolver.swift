36a7c85819db6d03277a7f44c14e05e610a99954
John Woo <wooj@amazon.com>
2021-11-03 14:53:23 -0700

    chore: improve developer experience with region resolver

--- AWSClientRuntime/Sources/Regions/DefaultRegionResolver.swift
+++ AWSClientRuntime/Sources/Regions/DefaultRegionResolver.swift
@@ -5,26 +5,32 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 import AwsCommonRuntimeKit
+import ClientRuntime
 
 public struct DefaultRegionResolver: RegionResolver {
     public let providers: [RegionProvider]
-    
+    let logger: SwiftLogger
+
     public init(providers: [RegionProvider] = [EnvironmentRegionProvider(), ProfileRegionProvider(), IMDSRegionProvider()]) {
         // TODO: add more region resolvers i.e. System Properties, etc
         self.providers = providers
+        self.logger = SwiftLogger(label: "DefaultRegionProvider")
     }
     
     public func resolveRegion() -> String? {
         for provider in providers {
             do {
                 // TODO: Verify that this does not block calling thread. If it does, we need to solve this similar to the way we solve this for the credentials provider
+                logger.debug("Attempting to resolve region with: \(String(describing: type(of: provider)))")
                 if let region = try provider.resolveRegion().get() {
+                    logger.debug("Resolved region with: \(String(describing: type(of: provider)))")
                     return region
                 }
             } catch {
                 return nil
             }
         }
+        logger.error("Unable to resolve region")
         return nil
     }
 }
