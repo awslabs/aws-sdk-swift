0df07443762c7e01513d68601556bcf6d07cd0e8
John Woo <1911939+wooj2@users.noreply.github.com>
2022-01-11 09:32:53 -0800

    chore: make signing flags public (#522)

--- AWSClientRuntime/Sources/Signing/SigningFlags.swift
+++ AWSClientRuntime/Sources/Signing/SigningFlags.swift
@@ -7,9 +7,15 @@
 import AwsCommonRuntimeKit
         
 public struct SigningFlags {
-    let useDoubleURIEncode: Bool
-    let shouldNormalizeURIPath: Bool
-    let omitSessionToken: Bool
+    public let useDoubleURIEncode: Bool
+    public let shouldNormalizeURIPath: Bool
+    public let omitSessionToken: Bool
+
+    public init(useDoubleURIEncode: Bool, shouldNormalizeURIPath: Bool, omitSessionToken: Bool) {
+        self.useDoubleURIEncode = useDoubleURIEncode
+        self.shouldNormalizeURIPath = shouldNormalizeURIPath
+        self.omitSessionToken = omitSessionToken
+    }
 }
 
 extension SigningFlags {
