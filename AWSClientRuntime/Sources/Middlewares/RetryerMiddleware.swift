a1e074f7873b16831b66d783b240dc0e182035f2
Nicki Stone <nickik@amazon.com>
2022-01-20 21:23:14 -0800

    john is a g

--- AWSClientRuntime/Sources/Middlewares/RetryerMiddleware.swift
+++ AWSClientRuntime/Sources/Middlewares/RetryerMiddleware.swift
@@ -22,28 +22,27 @@ public struct RetryerMiddleware<Output: HttpResponseBinding,
                           input: SdkHttpRequestBuilder,
                           next: H) async throws -> OperationOutput<Output>
     where H: Handler,
-    Self.MInput == H.Input,
-    Self.MOutput == H.Output,
-    Self.Context == H.Context {
-        
-        guard let region = context.getRegion(), !region.isEmpty else {
-            throw SdkError<OutputError>.client(ClientError.unknownError(("Region is unable to be resolved")))
-        }
-        do {
-            let partitionId = "\(context.getServiceName()) - \(region))"
-            let token = try await retryer.acquireToken(partitionId: partitionId)
-            return try await tryRequest(token: token,
-                                        partitionId: partitionId,
-                                        context: context,
-                                        input: input,
-                                        next: next)
-            
-        } catch let err {
-            throw SdkError<OutputError>.client(ClientError.retryError(err))
-        }
-    }
+          Self.MInput == H.Input,
+          Self.MOutput == H.Output,
+          Self.Context == H.Context {
+              
+              guard let region = context.getRegion(), !region.isEmpty else {
+                  throw SdkError<OutputError>.client(ClientError.unknownError(("Region is unable to be resolved")))
+              }
+              do {
+                  let partitionId = "\(context.getServiceName()) - \(region))"
+                  let token = try await retryer.acquireToken(partitionId: partitionId)
+                  return try await tryRequest(token: token,
+                                              partitionId: partitionId,
+                                              context: context,
+                                              input: input,
+                                              next: next)
+                  
+              } catch let err {
+                  throw SdkError<OutputError>.client(ClientError.retryError(err))
+              }
+          }
     
-    // swiftlint:disable force_cast
     func tryRequest<H>(token: RetryToken,
                        errorType: RetryError? = nil,
                        partitionId: String,
@@ -51,31 +50,28 @@ public struct RetryerMiddleware<Output: HttpResponseBinding,
                        input: SdkHttpRequestBuilder,
                        next: H) async throws -> OperationOutput<Output>
     where H: Handler,
-    Self.MInput == H.Input,
-    Self.MOutput == H.Output,
-    Self.Context == H.Context {
-        defer { retryer.releaseToken(token: token)}
-        do {
-            let serviceResponse = try await next.handle(context: context, input: input)
-            retryer.recordSuccess(token: token)
-            return serviceResponse
-        } catch let error where error is SdkError<OutputError> {
-            if retryer.isErrorRetryable(error: error as! SdkError<OutputError>) {
-                let errorType = retryer.getErrorType(error: error as! SdkError<OutputError>)
-                let newToken = try await retryer.scheduleRetry(token: token, error: errorType)
-                // TODO: rewind the stream once streaming is properly implemented
-                return try await tryRequest(token: newToken,
-                                            partitionId: partitionId,
-                                            context: context,
-                                            input: input,
-                                            next: next)
-            } else {
-                throw error
-            }
-        } catch let error {
-            throw error
-        }
-    }
+          Self.MInput == H.Input,
+          Self.MOutput == H.Output,
+          Self.Context == H.Context {
+              defer { retryer.releaseToken(token: token)}
+              do {
+                  let serviceResponse = try await next.handle(context: context, input: input)
+                  retryer.recordSuccess(token: token)
+                  return serviceResponse
+              } catch let error as SdkError<OutputError> where retryer.isErrorRetryable(error: error) {
+                  
+                  let errorType = retryer.getErrorType(error: error)
+                  let newToken = try await retryer.scheduleRetry(token: token, error: errorType)
+                  // TODO: rewind the stream once streaming is properly implemented
+                  return try await tryRequest(token: newToken,
+                                              partitionId: partitionId,
+                                              context: context,
+                                              input: input,
+                                              next: next)
+              } catch {
+                  throw error
+              }
+          }
     
     public typealias MInput = SdkHttpRequestBuilder
     public typealias MOutput = OperationOutput<Output>
