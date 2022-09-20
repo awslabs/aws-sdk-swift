b8c548fa6fd6426402ef9e11e79bbfd74dc8bf8e
Josh Elkins <jbelkins@amazon.com>
2022-09-20 17:36:03 -0500

    Correct ClientRuntime path in the AWSClientRuntime package manifest

--- AWSClientRuntime/Package.swift
+++ AWSClientRuntime/Package.swift
@@ -42,11 +42,11 @@ if let smithySwiftDir = ProcessInfo.processInfo.environment["SMITHY_SWIFT_CI_DIR
    let crtDir = ProcessInfo.processInfo.environment["AWS_CRT_SWIFT_CI_DIR"] {
     package.dependencies += [
         .package(name: "AwsCrt", path: "\(crtDir)"),
-        .package(name: "ClientRuntime", path: "\(smithySwiftDir)/Packages")
+        .package(name: "ClientRuntime", path: "\(smithySwiftDir)")
     ]
 } else {
     package.dependencies += [
         .package(name: "AwsCrt", path: "~/Projects/Amplify/SwiftSDK/aws-crt-swift"),
-        .package(name: "ClientRuntime", path: "~/Projects/Amplify/SwiftSDK/smithy-swift/Packages")
+        .package(name: "ClientRuntime", path: "~/Projects/Amplify/SwiftSDK/smithy-swift")
     ]
 }
