25fea412f1a72dd853c4ae762331bd027f1a4a8c
Ganesh Jangir <jangirg@amazon.com>
2022-09-28 14:39:53 -0700

    include all . files

--- Package.swift
+++ Package.swift
@@ -91,7 +91,7 @@ private extension Package {
 
         let sdksToIncludeInTargets = try! FileManager.default.contentsOfDirectory(atPath: localReleaseSwiftSDKDir.path)
             .filter { target in
-                !target.hasPrefix(".DS_Store")
+                !target.hasPrefix(".")
             }
         includeTargets(package: self, releasedSDKs: sdksToIncludeInTargets)
         return self
