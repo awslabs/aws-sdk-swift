666076690b9f71382089c8276d75f8264dbb8377
Ganesh Jangir <jangirg@amazon.com>
2022-10-07 11:17:46 -0700

    fix syntax

--- Package.swift
+++ Package.swift
@@ -327,8 +327,8 @@ let package = Package(
         .library(name: "AWSXRay", targets: ["AWSXRay"]),
     ],
     dependencies: [
-        .package(name: "AwsCrt", url: "https://github.com/awslabs/aws-crt-swift.git", exact: "0.2.2"),
-        .package(name: "ClientRuntime", url: "https://github.com/awslabs/smithy-swift.git", exact: "0.3.0")
+        .package(name: "AwsCrt", url: "https://github.com/awslabs/aws-crt-swift.git", .exact("0.2.2")),
+        .package(name: "ClientRuntime", url: "https://github.com/awslabs/smithy-swift.git", .exact("0.3.0"))
     ],
     targets: [
         .target(
