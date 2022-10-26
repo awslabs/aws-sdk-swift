4e2dea21808be965d396597f0230d51270462c0b
Josh Elkins <jbelkins@amazon.com>
2022-10-26 14:57:10 -0500

    Fix env var settings; build & test on release

---
+++ scripts/set_references.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -x
+
+# Intended to be run as part of a Github Actions job only.
+# Sets up env vars for use in Package.swift generation.
+
+# Determine if the current branch name exists on smithy-swift.
+# If so, set the env var with that branch name.
+# If no smithy-swift branch name matches the aws-sdk-swift branch name, default to main.
+if git ls-remote --exit-code --heads https://github.com/awslabs/smithy-swift.git "$AWS_SDK_REF_TO_BUILD"
+then
+  echo "AWS_SDK_SMITHY_SWIFT_BRANCH_OVERRIDE=$AWS_SDK_REF_TO_BUILD" >> $GITHUB_ENV
+else
+  echo "AWS_SDK_SMITHY_SWIFT_BRANCH_OVERRIDE=main" >> $GITHUB_ENV
+fi
+
+# Determine if the current branch name exists on aws-crt-swift.
+# If so, set the env var with that branch name.
+# If no aws-sdk-swift branch name matches the aws-sdk-swift branch name, default to main.
+if git ls-remote --exit-code --heads https://github.com/awslabs/aws-crt-swift.git "$AWS_SDK_REF_TO_BUILD"
+then
+  echo "AWS_SDK_AWS_CRT_SWIFT_BRANCH_OVERRIDE=$AWS_SDK_REF_TO_BUILD" >> $GITHUB_ENV
+else
+  echo "AWS_SDK_AWS_CRT_SWIFT_BRANCH_OVERRIDE=main" >> $GITHUB_ENV
+fi
