142180ad020e8b1e61249b87208a4139c4afb381
Ed Paulosky <eeppaauu@amazon.com>
2022-11-11 11:58:47 -0500

    Converts Expires members in S3 output structures to String shape

---
+++ codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/customization/s3/S3Expires.kt
@@ -0,0 +1,35 @@
+package software.amazon.smithy.aws.swift.codegen.customization.s3
+
+import software.amazon.smithy.model.Model
+import software.amazon.smithy.model.shapes.Shape
+import software.amazon.smithy.model.shapes.ServiceShape
+import software.amazon.smithy.model.traits.ClientOptionalTrait
+import software.amazon.smithy.model.transform.ModelTransformer
+import software.amazon.smithy.swift.codegen.SwiftSettings
+import software.amazon.smithy.swift.codegen.integration.SwiftIntegration
+import software.amazon.smithy.swift.codegen.model.defaultName
+import software.amazon.smithy.swift.codegen.model.expectShape
+
+class S3Expires: SwiftIntegration {
+    override val order: Byte
+        get() = 127
+
+    override fun enabledForService(model: Model, settings: SwiftSettings): Boolean {
+        return model.expectShape<ServiceShape>(settings.service).isS3
+    }
+    
+    override fun preprocessModel(model: Model, settings: SwiftSettings): Model {
+        // Find all the members named "Expires" in all of the output structures
+        // and change their shape from `Timestamp` to `String`
+        val updates = model.structureShapes
+            .filter { it.defaultName().endsWith("OutputResponse") }
+            .flatMap { it.allMembers.values }
+            .filter { it.memberName == "Expires" }
+            .map {
+                val builder = it.toBuilder()
+                builder.target("smithy.api#String")
+                builder.build()
+            }
+        return ModelTransformer.create().replaceShapes(model, updates)
+    }
+}
---
+++ codegen/smithy-aws-swift-codegen/src/test/kotlin/software/amazon/smithy/aws/swift/codegen/customizations/S3ExpiresTest.kt
@@ -0,0 +1,96 @@
+package software.amazon.smithy.aws.swift.codegen.customizations
+
+import io.kotest.matchers.string.shouldContainOnlyOnce
+import org.junit.jupiter.api.Test
+import software.amazon.smithy.aws.swift.codegen.TestContext
+import software.amazon.smithy.aws.swift.codegen.TestContextGenerator
+import software.amazon.smithy.aws.swift.codegen.restjson.AWSRestJson1ProtocolGenerator
+import software.amazon.smithy.aws.swift.codegen.shouldSyntacticSanityCheck
+import software.amazon.smithy.aws.traits.protocols.RestJson1Trait
+import software.amazon.smithy.swift.codegen.core.GenerationContext
+import software.amazon.smithy.swift.codegen.integration.ProtocolGenerator
+
+class S3ExpiresTest {
+
+    @Test
+    fun `001 test S3 output members named expires are changed to string type`() {
+        val context = setupTests("s3-expires.smithy", "com.amazonaws.s3#S3", "S3")
+        val contents = TestContextGenerator.getFileContents(context.manifest, "/Example/models/FooOutputResponse.swift")
+        contents.shouldSyntacticSanityCheck()
+        val expectedContents =
+            """
+            public struct FooOutputResponse: Swift.Equatable {
+                public var expires: Swift.String?
+                public var payload1: Swift.String?
+            
+                public init (
+                    expires: Swift.String? = nil,
+                    payload1: Swift.String? = nil
+                )
+                {
+                    self.expires = expires
+                    self.payload1 = payload1
+                }
+            }
+            """.trimIndent()
+        contents.shouldContainOnlyOnce(expectedContents)
+    }
+
+    @Test
+    fun `002 test S3 input members named expires are not changed`() {
+        val context = setupTests("s3-expires.smithy", "com.amazonaws.s3#S3", "S3")
+        val contents = TestContextGenerator.getFileContents(context.manifest, "/Example/models/FooInput.swift")
+        contents.shouldSyntacticSanityCheck()
+        val expectedContents =
+            """
+            public struct FooInput: Swift.Equatable {
+                public var expires: ClientRuntime.Date?
+                public var payload1: Swift.String?
+            
+                public init (
+                    expires: ClientRuntime.Date? = nil,
+                    payload1: Swift.String? = nil
+                )
+                {
+                    self.expires = expires
+                    self.payload1 = payload1
+                }
+            }
+            """.trimIndent()
+        contents.shouldContainOnlyOnce(expectedContents)
+    }
+
+    @Test
+    fun `003 test non-S3 output members named expires are not changed`() {
+        val context = setupTests("s3-expires.smithy", "com.amazonaws.s3#Bar", "Bar")
+        val contents = TestContextGenerator.getFileContents(context.manifest, "/Example/models/FooOutputResponse.swift")
+        contents.shouldSyntacticSanityCheck()
+        val expectedContents =
+            """
+            public struct FooOutputResponse: Swift.Equatable {
+                public var expires: ClientRuntime.Date?
+                public var payload1: Swift.String?
+            
+                public init (
+                    expires: ClientRuntime.Date? = nil,
+                    payload1: Swift.String? = nil
+                )
+                {
+                    self.expires = expires
+                    self.payload1 = payload1
+                }
+            }
+            """.trimIndent()
+        contents.shouldContainOnlyOnce(expectedContents)
+    }
+
+    private fun setupTests(smithyFile: String, serviceShapeId: String, sdkID: String): TestContext {
+        val context =
+            TestContextGenerator.initContextFrom(smithyFile, serviceShapeId, RestJson1Trait.ID, sdkID, sdkID)
+
+        val generator = AWSRestJson1ProtocolGenerator()
+        generator.generateProtocolUnitTests(context.ctx)
+        context.ctx.delegator.flushWriters()
+        return context
+    }
+}
