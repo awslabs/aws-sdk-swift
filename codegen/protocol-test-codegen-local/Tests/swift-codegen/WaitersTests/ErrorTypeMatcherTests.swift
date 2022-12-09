b4f89be28c04459970dfa132a34b8563af52c044
Josh Elkins <jbelkins@amazon.com>
2022-12-09 15:58:48 -0600

    Add real tests for error matchers

--- codegen/protocol-test-codegen-local/Tests/swift-codegen/WaitersTests/ErrorTypeMatcherTests.swift
+++ codegen/protocol-test-codegen-local/Tests/swift-codegen/WaitersTests/ErrorTypeMatcherTests.swift
@@ -10,15 +10,57 @@ import XCTest
 @testable import ClientRuntime
 
 #if swift(>=5.7)
+
 class ErrorTypeMatcherTests: XCTestCase {
+
+    // expected errorType for these tests: "MyError"
 
     // MARK: - errorType matcher
 
-    func test_errorType_neverMatchesUntilImplemented() async throws {
+    func test_errorType_matchesWhenErrorTypeMatchesAndErrorIsAWaiterTypedError() async throws {
+        let error = WaiterTypedErrorImpl()
         let subject = try WaitersClient.errorTypeMatcherWaiterConfig().acceptors[0]
-        let match = subject.evaluate(input: anInput, result: .failure("MyError"))
+        let match = subject.evaluate(input: anInput, result: .failure(error))
+        XCTAssertEqual(match, .success(.failure(error)))
+    }
+
+    func test_errorType_doesNotMatchWhenErrorTypeDoesNotMatchAndErrorIsAWaiterTypedError() async throws {
+        let error = WaiterTypedErrorNoMatchImpl()
+        let subject = try WaitersClient.errorTypeMatcherWaiterConfig().acceptors[0]
+        let match = subject.evaluate(input: anInput, result: .failure(error))
         XCTAssertNil(match)
     }
 
+    func test_errorType_doesNotMatchWhenErrorTypeMatchesButErrorIsNotAWaiterTypedError() async throws {
+        let error = WaiterTypedErrorNoMatchImpl()
+        let subject = try WaitersClient.errorTypeMatcherWaiterConfig().acceptors[0]
+        let match = subject.evaluate(input: anInput, result: .failure(error))
+        XCTAssertNil(match)
+    }
+
+    func test_errorType_doesNotMatchWhenResultIsSuccess() async throws {
+        let response = GetWidgetOutputResponse()
+        let subject = try WaitersClient.errorTypeMatcherWaiterConfig().acceptors[0]
+        let match = subject.evaluate(input: anInput, result: .success(response))
+        XCTAssertNil(match)
+    }
 }
+
+// Error types used in tests above
+
+private struct WaiterTypedErrorImpl: WaiterTypedError, Equatable {
+
+    var waiterErrorType: String? { "MyError" }
+}
+
+private struct WaiterTypedErrorNoMatchImpl: WaiterTypedError, Equatable {
+
+    var waiterErrorType: String? { "OtherError" }
+}
+
+private struct NotAWaiterTypedErrorImpl: Error, Equatable {  // An error but not a WaiterTypedError
+
+    var waiterErrorType: String? { "MyError" }
+}
+
 #endif
