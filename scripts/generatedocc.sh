f08383fe7059338ac03b6b5570d1e55a31f24d89
Ganesh Jangir <jangirg@amazon.com>
2022-12-27 16:52:03 -0800

    remove duplicate static content

--- scripts/generatedocc.sh
+++ scripts/generatedocc.sh
@@ -54,7 +54,7 @@ for package in $packages; do
             generate-documentation --target $package \
             --disable-indexing \
             --transform-for-static-hosting \
-            --hosting-base-path $VERSION/$package \
+            --hosting-base-path / \ # this is the base path for the css and js files, we don't want to duplicate them
             --output-path ./docs/$VERSION/$package
     
     # break if swift package generate-documentation fails
