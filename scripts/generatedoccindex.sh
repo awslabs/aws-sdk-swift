cb4123ae18e681b994b5cce925cf806eb0b7ea04
Ganesh Jangir <jangirg@amazon.com>
2022-12-28 19:46:39 -0800

    template

--- scripts/generatedoccindex.sh
+++ scripts/generatedoccindex.sh
@@ -75,5 +75,7 @@ sed -i '' "s/{{IDENTIFIERS}}/$identifiers/g" docs/$VERSION/awssdkswift/data/docu
 echo "Replacing {{REFERENCES}} in template"
 sed -i '' "s/{{REFERENCES}}/$references/g" docs/$VERSION/awssdkswift/data/documentation/awssdkswift.json
 echo "Replacing {{BASEURL}} in template"
-sed -i '' "s#{{BASEURL}}#$VERSION#g" docs/$VERSION/awssdkswift/documentation/awssdkswift/index.html
-sed -i '' "s#{{BASEURL}}#$VERSION#g" docs/$VERSION/awssdkswift/index.html
+# remove both leading and trailing slash
+BASEURL=$(echo $VERSION | sed 's/^\///' | sed 's/\/$//')
+sed -i '' "s#{{BASEURL}}#$BASEURL#g" docs/$VERSION/awssdkswift/documentation/awssdkswift/index.html
+sed -i '' "s#{{BASEURL}}#$BASEURL#g" docs/$VERSION/awssdkswift/index.html
