40e66dd6bbd7d852d02e0244c05fc83a75bfe0e5
Ed Paulosky <eeppaauu@amazon.com>
2023-01-05 16:08:40 -0500

    fix: Fixes S3 and Ec2 error parsing (#787)
    * Fixes up s3 error integration
    
    * Updates requestId getters
    
    * Fixes typo
    
    * Handles alternative casing for RequestId property in Ec2 errors
    
    * ktlintformat
    
    * Adds docs

---
+++ AWSClientRuntime/Sources/Http/HttpResponse+AWS.swift
@@ -0,0 +1,23 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+import ClientRuntime
+
+public extension HttpResponse {
+    /// Returns true if the status code is `HttpStatusCode.notFound` and the body is empty.
+    var statusCodeIsNotFoundAndBodyIsEmpty: Bool {
+        return statusCode == .notFound && body.isEmpty
+    }
+    
+    /// The value of the x-amz-request-id header.
+    var requestId: String? {
+        return headers.value(for: "x-amz-request-id")
+    }
+    
+    /// The value of the x-amz-id-2 header.
+    var requestId2: String? {
+        return headers.value(for: "x-amz-id-2")
+    }
+}
