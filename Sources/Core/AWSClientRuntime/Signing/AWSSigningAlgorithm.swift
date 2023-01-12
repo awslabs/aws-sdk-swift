254ad26db7fc74385e93db4e87c3ca55232395ac
Ganesh Jangir <jangirg@amazon.com>
2023-01-12 11:26:59 -0800

    add docs

--- Sources/Core/AWSClientRuntime/Signing/AWSSigningAlgorithm.swift
+++ Sources/Core/AWSClientRuntime/Signing/AWSSigningAlgorithm.swift
@@ -6,12 +6,19 @@
 //
 import AwsCommonRuntimeKit
 
+
+/// Type of signing algorithm
 public enum AWSSigningAlgorithm: String {
+    ///  Signature Version 4
     case sigv4
+    ///  Signature Version 4 Asymmetric
     case sigv4a
 }
 
 extension AWSSigningAlgorithm {
+
+    /// Convert self to CRT SigningAlgorithmType
+    /// - Returns: SigningAlgorithmType
     func toCRTType() -> SigningAlgorithmType {
         switch self {
         case .sigv4: return .signingV4
