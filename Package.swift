f7245d1c1bc9482f844d3b839704b01d1965c654
Ganesh Jangir <jangirg@amazon.com>
2023-01-31 17:31:01 -0800

    chore: bump aws-crt-swift to 0.5.7 (#863)

--- Package.swift
+++ Package.swift
@@ -364,7 +364,7 @@ let package = Package(
         .library(name: "AWSXRay", targets: ["AWSXRay"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/awslabs/aws-crt-swift", .exact("0.5.4"))
+        .package(url: "https://github.com/awslabs/aws-crt-swift", .exact("0.5.7"))
     ],
     targets: [
         // MARK: - Core Targets
@@ -1070,6 +1070,6 @@ case (false, true):
     ]
 case (false, false):
     package.dependencies += [
-        .package(url: "https://github.com/awslabs/smithy-swift", .exact("0.10.2"))
+        .package(url: "https://github.com/awslabs/smithy-swift", .exact("0.10.3"))
     ]
 }
