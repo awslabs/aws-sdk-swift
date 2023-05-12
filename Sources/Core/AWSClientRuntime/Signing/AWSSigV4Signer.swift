65475df79b7e0a2de328428f979c0267c386a47e
Josh Elkins <jbelkins@amazon.com>
2023-05-12 13:16:43 -0500

    Escape in right place

--- Sources/Core/AWSClientRuntime/Signing/AWSSigV4Signer.swift
+++ Sources/Core/AWSClientRuntime/Signing/AWSSigV4Signer.swift
@@ -82,7 +82,7 @@ public class AWSSigV4Signer {
     ) async -> SdkHttpRequest? {
         let originalRequest = requestBuilder.build()
         do {
-            let crtUnsignedRequest = try originalRequest.toHttpRequest(escaping: true)
+            let crtUnsignedRequest = try originalRequest.toHttpRequest()
 
             let crtSignedRequest = try await Signer.signRequest(
                 request: crtUnsignedRequest,
