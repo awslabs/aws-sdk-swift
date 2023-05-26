806aaf276520c621b446a318c624d8dc44f56465
Josh Elkins <jbelkins@amazon.com>
2023-05-25 19:11:03 -0500

    Test for xcodebuild install before logging version

--- scripts/ci_steps/log_tool_versions.sh
+++ scripts/ci_steps/log_tool_versions.sh
@@ -3,11 +3,19 @@
 set -e
 
 # Log location & version for swiftc, xcodebuild, java
+
 which swiftc
 swiftc --version
 echo
-which xcodebuild
-xcodebuild -version
-echo
+
+if command -v xcodebuild &> /dev/null
+then
+  which xcodebuild
+  xcodebuild -version
+  echo
+else
+  echo "xcodebuild not installed"
+fi
+
 which java
 java --version
