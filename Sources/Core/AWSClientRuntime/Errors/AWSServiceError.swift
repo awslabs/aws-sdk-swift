a30c26f588665fc444af1b261317e1bdc95ec3d3
Josh Elkins <jbelkins@amazon.com>
2023-06-01 13:04:44 -0500

    Add comments

--- Sources/Core/AWSClientRuntime/Errors/AWSServiceError.swift
+++ Sources/Core/AWSClientRuntime/Errors/AWSServiceError.swift
@@ -3,8 +3,11 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
-import ClientRuntime
+import protocol ClientRuntime.ServiceError
 
+/// Provides properties for an error returned by an AWS service.
 public protocol AWSServiceError: ServiceError {
+
+    /// The Request ID for a request made to an AWS service, if available.
     var requestID: String? { get }
 }
