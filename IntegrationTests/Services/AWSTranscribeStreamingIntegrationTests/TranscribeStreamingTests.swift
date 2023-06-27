e8a0a6075e0a5fe0621986c93ae7f2c230d18db8
Josh Elkins <jbelkins@amazon.com>
2023-06-27 12:02:17 -0500

    Test tweaks

--- IntegrationTests/Services/AWSTranscribeStreamingIntegrationTests/TranscribeStreamingTests.swift
+++ IntegrationTests/Services/AWSTranscribeStreamingIntegrationTests/TranscribeStreamingTests.swift
@@ -26,7 +26,7 @@ final class TranscribeStreamingTests: XCTestCase {
                 var currentEnd = min(chunkSize, audioDataSize - currentStart)
 
                 while currentStart < audioDataSize {
-                    try await Task.sleep(nanoseconds: 3_000_000_000)
+                    try await Task.sleep(nanoseconds: 1_000_000_000)
                     let dataChunk = audioData[currentStart ..< currentEnd]
 
                     let audioEvent =  TranscribeStreamingClientTypes.AudioStream.audioevent(.init(audioChunk: dataChunk))
