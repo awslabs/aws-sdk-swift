cd1f9aa9dcc49a6bb8b6abb19fdc69e122a38b84
Josh Elkins <jbelkins@amazon.com>
2023-09-14 15:22:13 -0500

    Update to use refactored Smithy endpoint rules engine

--- codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/EndpointResolverGenerator.kt
+++ codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/EndpointResolverGenerator.kt
@@ -8,8 +8,10 @@ package software.amazon.smithy.aws.swift.codegen
 import software.amazon.smithy.aws.swift.codegen.middleware.EndpointResolverMiddleware
 import software.amazon.smithy.codegen.core.Symbol
 import software.amazon.smithy.model.node.Node
+import software.amazon.smithy.rulesengine.aws.language.functions.AwsPartition
+import software.amazon.smithy.rulesengine.aws.language.functions.partition.Partitions
 import software.amazon.smithy.rulesengine.language.EndpointRuleSet
-import software.amazon.smithy.rulesengine.language.stdlib.partition.DefaultPartitionDataProvider
+//import software.amazon.smithy.rulesengine.language.stdlib.partition.DefaultPartitionDataProvider
 import software.amazon.smithy.rulesengine.traits.EndpointRuleSetTrait
 import software.amazon.smithy.swift.codegen.ClientRuntimeTypes
 import software.amazon.smithy.swift.codegen.MiddlewareGenerator
@@ -59,7 +61,7 @@ class EndpointResolverGenerator() {
             writer.write("")
             endpointRules?.let {
                 writer.write("private let engine: \$L", AWSClientRuntimeTypes.Core.AWSEndpointsRuleEngine)
-                val partitions = DefaultPartitionDataProvider::class.java.getResourceAsStream("/software/amazon/smithy/rulesengine/language/partitions.json")
+                val partitions = AwsPartition::class.java.getResourceAsStream("/software/amazon/smithy/rulesengine/aws/language/functions/partition/partitions.json")
                 writer.write("private let partitions = \$S", Node.printJson(Node.parse(partitions)))
                 writer.write("private let ruleSet = \$S", Node.printJson(endpointRules.toNode()))
             }
