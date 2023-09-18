a5d395ecd0e67abc933df54aa73abbd95bfe3d57
Sichan Yoo <chanyoo@amazon.com>
2023-09-17 21:45:27 -0700

    Add permission set generation.

--- IntegrationTests/Services/AWSS3IntegrationTests/SSOCredentialsProviderTests.swift
+++ IntegrationTests/Services/AWSS3IntegrationTests/SSOCredentialsProviderTests.swift
@@ -13,45 +13,43 @@ import AWSSSOAdmin
 
 /* Prerequisites to run the SSO credentials provider integration test(s):
  *
- * 1. Enable IAM Identity Center AWS Organization for the AWS account used for integration test if it hasn't been already.
- * 2. Create a permission set (for S3::listBuckets) in IAM Identity Center.
- * 3. Create a user in IAM Identity Center, and retrieve username, password, and SSO start URL (these are needed for step 4, aws sso configure).
+ * [Manual]
+ * 1. Enable IAM Identity Center AWS Organization for the AWS account used to run integration test if it hasn't been already.
+ *
+ * [Automatic]
+ * 2. Create a read-only permission set in IAM Identity Center called "SSOCredProvIntegTestPermSet".
+ 
+ * [Manual]
+ * 3. Create a user in IAM Identity Center.
  * 4. Give the created user access to your AWS account with the created permission set.
- * 5. Configure SSO config.
+ * 5. Retrieve username, password, and SSO start URL of the IAM Identity Store user. Configure SSO config using these values (aws sso configure).
  * 6. Create SSO token using AWS CLI (aws sso login --profile <profile-name-set-during-aws-sso-configure>).
  * 7. Run the test.
  *
- * Only step 1 has to be done manually in the AWS account console you're using to run this integration test.
+ * Note: Enabling AWS organization and creating IAM identity store have no exposed API as of 9/17/2023 for programmatic activation / creation.
+ *
+ * Step 1, 3, 4, 5 have to be done only once per account.
  */
 class SSOCredentialsProviderTests : XCTestCase {
     var client: S3Client!
     var ssoClient: SSOAdminClient!
     
-    private var username: String = ""
-    private var password: String = ""
-    private var startUrl: String = ""
-    
     private var iamIdentityCenterInstanceArn: String = ""
+    private let permissionSetName = "SSOCredProvIntegTestPermSet"
     private var permissionSetArn: String = ""
-    private var userArn: String = ""
+    private let awsReadOnlyPolicy = "arn:aws:iam::aws:policy/ReadOnlyAccess"
     
         
     override func setUp() async throws {
-        // Use default credentials provider chain
+        // Use default credentials provider chain for setup
         ssoClient = try await SSOClient()
-        try await createPermissionSet()
-        try await createUser()
-        try await giveAccountAccessToUserUsingPermissionSet()
+        try await createPermissionSetIfNeeded()
         
-        // Use AWS CLI for these
-        try await configureSSOConfig()
-        try await ssoLoginAndCreateToken()
-        
-        // This client uses SSO credentials provider to call S3::listBuckets operation
+        // Create a S3 client that uses SSO credentials provider
         try await setUpClient()
     }
 
-    // This test calls listBuckets() and forces S3Client to use SSOCredentialsProvider
+    // The test calls listBuckets() and forces S3Client to use SSOCredentialsProvider
     func test_listBuckets() async throws {
         _ = try await client.listBuckets(input: ListBucketsInput())
     }
@@ -61,36 +59,43 @@ class SSOCredentialsProviderTests : XCTestCase {
     }
     
     /* HELPER METHODS */
-    private func createPermissionSet() async throws {
-        if let response = try await ssoClient.listInstances(input: ListInstancesInput()) as! ListInstancesOutputResponse, response.instances != nil {
-            iamIdentityCenterInstanceArn = response.instances
-            
+    private func createPermissionSetIfNeeded() async throws {
+        // Get IAM identity center instanceArn
+        if let response = try await ssoClient.listInstances(input: ListInstancesInput()) as? ListInstancesOutputResponse {
+            iamIdentityCenterInstanceArn = response.instances![0].instanceArn
         }
         
-    }
-    
-    private func createUser() async throws {
+        // Check if permission set for this integ test is already present. Save its arn and return if it already exists.
+        let output = try await ssoClient.listPermissionSets(input: ListPermissionSetsInput(instanceArn: iamIdentityCenterInstanceArn)) as? ListPermissionSetsOutputResponse
+        guard let output = output, sets = output.permissionSets else {
+            throw Error("Failed to retrieve list of pre-existing permission sets from IAM Identity Center.")
+        }
+        if sets.contains(permissionSetName) {
+            // Set permission set instanceArn
+            let idx = sets.firstIndex(where: $0 == permissionSetName)
+            permissionSetArn = sets[idx]
+            return
+        }
         
-    }
-    
-    private func giveAccountAccessToUserUsingPermissionSet() async throws {
+        // Otherwise, create permission set and save its ARN
+        if let response = try await ssoClient.createPermissionSet(input: CreatePermissionSetInput(
+                description: "Permission set for testing SSO credentials provider.",
+                instanceArn: iamIdentityCenterInstanceArn,
+                name: permissionSetName
+        )) as? CreatePermissionSetOutputResponse {
+            permissionSetArn = response.permissionSet!.permissionSetArn!
+        }
         
-    }
-    
-    private func configureSSOConfig() async throws {
-        
-    }
-    
-    private func ssoLoginAndCreateToken() async throws {
-        
+        // Attach ReadOnly AWS-managed policy to the created permission set
+        let _ = try await ssoClient.attachManagedPolicyToPermissionSet(input: AttachManagedPolicyToPermissionSetInput(
+            instanceArn: iamIdentityCenterInstanceArn,
+            managedPolicyArn: awsReadOnlyPolicy,
+            permissionSetArn: permissionSetArn))
     }
     
     private func setUpClient() async throws {
         // Setup SSOCredentialsProvider
-        let SSOCredentialsProvider = try SSOCredentialsProvider(
-            configFilePath: Bundle.module.path(forResource: "sso-config", ofType: nil)!,
-            credentialsFilePath: Bundle.module.path(forResource: "sso-credentials", ofType: nil)!
-        )
+        let SSOCredentialsProvider = try SSOCredentialsProvider()
 
         // Setup S3ClientConfiguration to use SSOCredentialsProvider
         let testConfig = try await S3Client.S3ClientConfiguration()
