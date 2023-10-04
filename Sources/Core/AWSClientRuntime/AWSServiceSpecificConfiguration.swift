77ea1fcb77da79d79ee52ad9a7c8470a68a8acd3
Sichan Yoo <chanyoo@amazon.com>
2023-10-04 13:28:19 -0700

    New config fields & auth scheme struct

--- Sources/Core/AWSClientRuntime/AWSServiceSpecificConfiguration.swift
+++ Sources/Core/AWSClientRuntime/AWSServiceSpecificConfiguration.swift
@@ -5,6 +5,8 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+import ClientRuntime
+
 /// Contains config properties specific to one AWS service
 ///
 /// A custom service-specific configuration that conforms to this protocol will be code-generated
@@ -27,6 +29,16 @@ public protocol AWSServiceSpecificConfiguration {
     ///
     /// If none is provided at compile time, the service will provide one.
     var endpointResolver: AWSServiceEndpointResolver { get }
+    
+    /// List of configured auth schemes for the service.
+    ///
+    /// If none is provided at compile time, value will default to auth schemes supported by the service as defined in its model.
+    var authSchemes: Array<ClientRuntime.AuthScheme> { get }
+    
+    /// The auth scheme resolver to use for the service.
+    ///
+    /// If none is provided at compile time, value will default to default auth scheme resolver for the service generated based on its model.
+    var authSchemeResolver: ClientRuntime.AuthSchemeResolver { get }
 
     /// Creates a service-specific configuration for this service
     /// - Parameter endpointResolver: An endpoint resolver for the service, or `nil` to let
---
+++ Sources/Core/AWSClientRuntime/Auth/AuthSchemes/SigV4AuthScheme.swift
@@ -0,0 +1,15 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import Foundation
+import ClientRuntime
+
+public struct SigV4AuthScheme: ClientRuntime.AuthScheme {
+    public let schemeId: String = "aws.auth#sigv4"
+    public let signer: any ClientRuntime.Signer = AWSSigV4Signer()
+    public let idType: ClientRuntime.IdentityType = .aws
+}
