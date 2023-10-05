bc895f58b9563c19c0d9c450b4a4d5325f6d8cd2
Sichan Yoo <chanyoo@amazon.com>
2023-10-04 19:05:17 -0700

    Add signing properties customization hook implementation to sigv4 auth scheme.

--- Sources/Core/AWSClientRuntime/Auth/AuthSchemes/SigV4AuthScheme.swift
+++ Sources/Core/AWSClientRuntime/Auth/AuthSchemes/SigV4AuthScheme.swift
@@ -12,4 +12,11 @@ public struct SigV4AuthScheme: ClientRuntime.AuthScheme {
     public let schemeId: String = "aws.auth#sigv4"
     public let signer: any ClientRuntime.Signer = AWSSigV4Signer()
     public let idType: ClientRuntime.IdentityType = .aws
+    
+    public func customizeSigningProperties(signingProperties: Attributes, config: HttpContext) -> Attributes {
+        var copy = signingProperties
+        copy.set(key: AttributeKeys.bidirectionalStreaming, value: config.isBidirectionalStreamingEnabled())
+        copy.set(key: AttributeKeys.signingRegion, value: config.getSigningRegion())
+        return copy
+    }
 }
