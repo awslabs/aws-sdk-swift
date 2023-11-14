75fc81a482ebb99c65a9eb7bfac49990b432d9d5
Sichan Yoo <chanyoo@amazon.com>
2023-11-14 09:23:38 -0800

    Remove duplicate presign flag logic.

--- Sources/Core/AWSClientRuntime/Auth/AuthSchemes/SigV4AAuthScheme.swift
+++ Sources/Core/AWSClientRuntime/Auth/AuthSchemes/SigV4AAuthScheme.swift
@@ -17,33 +17,39 @@ public struct SigV4AAuthScheme: ClientRuntime.AuthScheme {
     public func customizeSigningProperties(signingProperties: Attributes, context: HttpContext) -> Attributes {
         var updatedSigningProperties = signingProperties
 
+        // Set signing algorithm flag
         updatedSigningProperties.set(key: AttributeKeys.signingAlgorithm, value: .sigv4a)
 
+        // Set bidirectional streaming flag
         updatedSigningProperties.set(
             key: AttributeKeys.bidirectionalStreaming,
             value: context.isBidirectionalStreamingEnabled()
         )
+
+        // Set signing name and signing region flags
         updatedSigningProperties.set(key: AttributeKeys.signingName, value: context.getSigningName())
         updatedSigningProperties.set(key: AttributeKeys.signingRegion, value: context.getSigningRegion())
 
+        // Set expiration flag
+        //
         // Expiration is only used for presigning (presign request flow or presign URL flow).
         updatedSigningProperties.set(key: AttributeKeys.expiration, value: context.getExpiration())
 
+        // Set signature type flag
+        //
         // AWSSignatureType.requestQueryParams is only used for presign URL flow.
         // Out of the AWSSignatureType enum cases, only two are used. .requestHeaders and .requestQueryParams.
         // .requestHeaders is the deafult signing used for AWS operations.
         let serviceName = context.getServiceName()
-        // Operation name is guaranteed to be in middleware context from generic codegen.
-        let operationName = context.getOperation()!
-        let isPresignServiceAndOpPair = SigV4Util.shouldPresignURL(serviceName: serviceName, opName: operationName)
         let isPresignURLFlow = context.getFlowType() == .PRESIGN_URL
-        let shouldUseRequestQueryParams = isPresignURLFlow && isPresignServiceAndOpPair
         updatedSigningProperties.set(
             key: AttributeKeys.signatureType,
-            value: shouldUseRequestQueryParams ? .requestQueryParams : .requestHeaders
+            value: isPresignURLFlow ? .requestQueryParams : .requestHeaders
         )
 
         // Set unsignedBody flag
+        // Operation name is guaranteed to be in middleware context from generic codegen.
+        let operationName = context.getOperation()!
         let shouldForceUnsignedBody = SigV4Util.shouldForceUnsignedBody(
             flow: context.getFlowType(),
             serviceName: serviceName,
@@ -59,8 +65,8 @@ public struct SigV4AAuthScheme: ClientRuntime.AuthScheme {
             value: useSignedBodyHeader ? .contentSha256 : AWSSignedBodyHeader.none
         )
 
-        // Flags in SigningFlags object
         let serviceIsS3 = serviceName == "S3"
+        // Set flags in SigningFlags object
         // Set useDoubleURIEncode to false IFF service is S3
         updatedSigningProperties.set(key: AttributeKeys.useDoubleURIEncode, value: !serviceIsS3)
         // Set shouldNormalizeURIPath to false IFF service is S3
--- Sources/Core/AWSClientRuntime/Auth/AuthSchemes/SigV4Util.swift
+++ Sources/Core/AWSClientRuntime/Auth/AuthSchemes/SigV4Util.swift
@@ -13,22 +13,11 @@ public class SigV4Util {
     static let forceUnsignedBodyForPresigningURL = [
         "S3": ["getObject", "putObject"]
     ]
-    // Update the presignURL variable in this helper function to add support for presigned URL
-    //  to a new operation in a service.
-    static let presignURL = [
-        "S3": ["getObject", "putObject"],
-        "Polly": ["synthesizeSpeech"]
-    ]
 
     static func shouldForceUnsignedBody(flow: FlowType, serviceName: String, opName: String) -> Bool {
         let serviceQualifies = forceUnsignedBodyForPresigningURL.keys.contains(serviceName)
         let flowQualies = flow == .PRESIGN_URL
         return serviceQualifies && flowQualies && forceUnsignedBodyForPresigningURL[serviceName]!.contains(opName)
-    }
-
-    static func shouldPresignURL(serviceName: String, opName: String) -> Bool {
-        let serviceQualifies = presignURL.keys.contains(serviceName)
-        return serviceQualifies && presignURL[serviceName]!.contains(opName)
     }
 
     static func serviceUsesUnsignedBodyHeader(serviceName: String) -> Bool {
