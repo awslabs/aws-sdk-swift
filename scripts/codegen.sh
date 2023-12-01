88779ffdc05f8af909eba9c2df642f12f8bcc456
Josh Elkins <jbelkins@users.noreply.github.com>
2023-12-01 16:03:45 -0600

    chore: Codegen dev script takes args for manifest generator (#1246)

--- scripts/codegen.sh
+++ scripts/codegen.sh
@@ -1,9 +1,10 @@
-#!/bin/bash 
+#!/bin/bash
 
 # Stop on any failed step of this script
 set -eo pipefail
 
 # Regenerates the SDK.  For use during development only.
+# Arguments passed into this script are passed on to the manifest generator.
 
 # May be used on Mac or Linux.
 # When run on Mac, kills Xcode before codegen & restarts it after.
@@ -28,9 +29,9 @@ rm -rf Tests/Services/*
 # Merge the newly built models
 ./scripts/mergeModels.sh Sources/Services
 
-# Regenerate the package manifest and doc index
+# Regenerate the package manifest and doc index, with args passed into this script
 cd AWSSDKSwiftCLI
-swift run AWSSDKSwiftCLI generate-package-manifest ..
+swift run AWSSDKSwiftCLI generate-package-manifest "$@" ..
 swift run AWSSDKSwiftCLI generate-doc-index ..
 cd ..
 
