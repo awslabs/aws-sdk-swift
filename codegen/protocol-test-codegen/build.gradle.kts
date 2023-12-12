a22eedd0a9e952e4583a67af9c35a08cf2ac7dc2
David Yaffe <dayaffe@amazon.com>
2023-12-12 16:37:46 -0500

    add exclusion for 'defaults' tag

--- codegen/protocol-test-codegen/build.gradle.kts
+++ codegen/protocol-test-codegen/build.gradle.kts
@@ -103,6 +103,12 @@ fun generateSmithyBuild(tests: List<ProtocolTest>): String {
                       "${test.serviceShapeId}"
                     ]
                   }
+                },
+                {
+                  "name": "excludeTraitsByTag",
+                  "args": {
+                    "tags": ["defaults"]
+                  }
                 }
               ],
               "plugins": {
