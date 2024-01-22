b2e5d3c7c8e09275c27121be11ae91c8e7de74ec
Sichan Yoo <chanyoo@amazon.com>
2024-01-22 14:39:45 -0800

    Finish SigV4A integration tests that use S3 MRAP.

--- IntegrationTests/Services/AWSS3IntegrationTests/S3SigV4ATests.swift
+++ IntegrationTests/Services/AWSS3IntegrationTests/S3SigV4ATests.swift
@@ -14,38 +14,42 @@ import AWSClientRuntime
 
 /// Tests SigV4A signing flow using S3's Multi-Region Access Point (MRAP).
 class S3SigV4ATests: S3XCTestCase {
-    // The custom S3 client configured with only SigV4A (w/o SigV4)
-    private var sigv4aS3Client: S3Client!
-    private var s3Config: S3Client.S3ClientConfiguration!
+    // The custom S3 client configured with only SigV4A auth scheme
+    // Used to put object to bucket via MRAP
+    private var sigv4aClient: S3Client!
+    private var sigv4aConfig: S3Client.S3ClientConfiguration!
 
     // MRAP ARN format
-    // arn:aws:s3::account-id:accesspoint/MultiRegionAccessPoint_alias
+    // arn:aws:s3::\{account-id}:accesspoint/\{MultiRegionAccessPoint_alias}
     private var mrapArnFormat = "arn:aws:s3::%@:accesspoint/%@"
-    private var mrapAlias: String!
     private var mrapArn: String!
+    private var mrapAlias: String!
     private let mrapName = "aws-sdk-s3-integration-test-" + UUID().uuidString.split(separator: "-").first!.lowercased()
 
-    // The S3 control client for creating and deleting MRAP
+    // The S3 control client used to create and delete MRAP
     private var s3ControlClient: S3ControlClient!
     
     // The STS client for fetching AWS account ID
     private var stsClient: STSClient!
     private var accountId: String!
 
+    // Key string used for putting object in tests
+    private let key = "text.txt"
+
     override func setUp() async throws {
         // Create a bucket
         try await super.setUp()
 
-        // Create a sigv4a-only S3 client
-        s3Config = try await S3Client.S3ClientConfiguration(region: region)
-        s3Config.authSchemes = [SigV4AAuthScheme()]
-        sigv4aS3Client = S3Client(config: s3Config)
+        // Create sigv4a-only S3 client
+        sigv4aConfig = try await S3Client.S3ClientConfiguration(region: region)
+        sigv4aConfig.authSchemes = [SigV4AAuthScheme()]
+        sigv4aClient = S3Client(config: sigv4aConfig)
 
         // Fetch account ID
         stsClient = try STSClient(region: region)
         accountId = try await stsClient.getCallerIdentity(input: GetCallerIdentityInput()).account
 
-        // Construct container with MRAP config values
+        // Construct MRAP config
         let publicAccessBlock = S3ControlClientTypes.PublicAccessBlockConfiguration(
             blockPublicAcls: false,
             blockPublicPolicy: false,
@@ -59,7 +63,7 @@ class S3SigV4ATests: S3XCTestCase {
             regions: regions
         )
 
-        // Create a S3 Multi-Region Access Point (MRAP)
+        // Create S3 Multi-Region Access Point (MRAP)
         s3ControlClient = try S3ControlClient(region: region)
         let createMRAPInput = CreateMultiRegionAccessPointInput(
             accountId: accountId,
@@ -81,6 +85,7 @@ class S3SigV4ATests: S3XCTestCase {
         } while status == .creating
 
         // Fetch MRAP alias then format & save MRAP ARN
+        // This MRAP ARN is used in place of bucket name in subsequent calls
         mrapAlias = try await s3ControlClient.getMultiRegionAccessPoint(input: GetMultiRegionAccessPointInput(
             accountId: accountId,
             name: mrapName
@@ -101,7 +106,7 @@ class S3SigV4ATests: S3XCTestCase {
             accountId: accountId,
             clientToken: UUID().uuidString.split(separator: "-").first!.lowercased(),
             details: S3ControlClientTypes.DeleteMultiRegionAccessPointInput(name: mrapName)
-        )).requestTokenARN
+        ))
 
         // Wait until MRAP has been deleted before returning
         var mrapCount: Int? = 1
@@ -114,9 +119,69 @@ class S3SigV4ATests: S3XCTestCase {
     }
 
     func testS3MRAPSigV4A() async throws {
-        // Call listObjectsV2 with SigV4A-only S3 client
-        let response = try await sigv4aS3Client.listObjectsV2(input: ListObjectsV2Input(bucket: mrapArn))
-        // Confirm response was returned
-        XCTAssertTrue(response.keyCount == 0)
+        // Put an object to bucket via MRAP using SigV4A-only client
+        _ = try await sigv4aClient.putObject(input: PutObjectInput(
+            body: ByteStream.data(Data()),
+            bucket: mrapArn,
+            key: key,
+            metadata: ["filename": key]
+        ))
+
+        // Confirm object has been successfully uploaded via MRAP
+        let response = try await sigv4aClient.listObjectsV2(
+            input: ListObjectsV2Input(bucket: mrapArn)
+        )
+        XCTAssertTrue(response.keyCount == 1)
+    }
+
+    func testS3MRAPSigV4APresignedRequest() async throws {
+        // Construct input
+        let putObjectInput = PutObjectInput(
+            body: .noStream,
+            bucket: mrapArn,
+            key: key,
+            metadata: ["filename": key]
+        )
+
+        // Presign the input with SigV4A (sigv4aConfig has only SigV4A configured)
+        let presignedRequest = try await putObjectInput.presign(
+            config: sigv4aConfig,
+            expiration: 60
+        )
+        guard let presignedRequest else {
+            XCTFail("Presigning PutObjectInput failed.")
+            // return added for compiler to not complain.
+            return
+        }
+
+        // Execute request
+        _ = try await sigv4aConfig.httpClientEngine.send(request: presignedRequest)
+
+        // Confirm object has been uploaded via MRAP
+        let response = try await sigv4aClient.listObjectsV2(
+            input: ListObjectsV2Input(bucket: mrapArn)
+        )
+        XCTAssertTrue(response.keyCount == 1)
+    }
+
+    func testS3MRAPSigV4APresignedURL() async throws {
+        // Construct input with bucketName set to mrapArn to send request to MRAP
+        let putObjectInput = PutObjectInput(body: .data(Data()), bucket: mrapArn, key: key, metadata: ["filename": key])
+
+        // Get presigned URL using SigV4A (sigv4aConfig has only SigV4A configured)
+        let presignedURLOrNil = try await putObjectInput.presignURL(config: sigv4aConfig, expiration: 30.0)
+        let presignedURL = try XCTUnwrap(presignedURLOrNil)
+
+        // Construct then send URL request using presigned URL
+        var urlRequest = URLRequest(url: presignedURL)
+        urlRequest.httpMethod = "PUT"
+        urlRequest.httpBody = Data()
+        _ = try await perform(urlRequest: urlRequest)
+
+        // Confirm object has been uploaded via MRAP
+        let response = try await sigv4aClient.listObjectsV2(
+            input: ListObjectsV2Input(bucket: mrapArn)
+        )
+        XCTAssertTrue(response.keyCount == 1)
     }
 }
