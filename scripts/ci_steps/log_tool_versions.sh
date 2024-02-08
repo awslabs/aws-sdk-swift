f79cd3538d816c8a4449972ecd37f625e21795a9
Josh Elkins <jbelkins@amazon.com>
2024-02-08 14:49:56 -0600

    Log CPU for hardware in use

--- scripts/ci_steps/log_tool_versions.sh
+++ scripts/ci_steps/log_tool_versions.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+# Log CPU for hardware in use, if running on Mac
+
+if command -v sysctl &> /dev/null
+then
+  sysctl -a | grep machdep.cpu
+  echo
+else
+  echo "sysctl not installed (not a Mac)"
+fi
+
 # Log location & version for swiftc, xcodebuild, java, xcbeautify
 
 if command -v swiftc &> /dev/null
