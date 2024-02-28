cc4c658a4db62abd8de03fa67ba8d88073dbd12d
Sichan Yoo <chanyoo@amazon.com>
2024-02-28 14:23:50 -0800

    Rename IMDSCredentialsProvider to IMDSAWSCredentialIdentityResolver & update var names in implementation.

--- Sources/Core/AWSClientRuntime/Auth/AWSCredentialIdentityResolvers/IMDSAWSCredentialIdentityResolver.swift
+++ Sources/Core/AWSClientRuntime/Auth/AWSCredentialIdentityResolvers/IMDSAWSCredentialIdentityResolver.swift
@@ -10,13 +10,15 @@ import ClientRuntime
 import Foundation
 
 /// A credentials provider that uses IMDSv2 to fetch credentials within an EC2 instance.
-public struct IMDSCredentialsProvider: CredentialsSourcedByCRT {
-    let crtCredentialsProvider: CRTCredentialsProvider
+public struct IMDSAWSCredentialIdentityResolver: AWSCredentialIdentityResolvedByCRT {
+    let crtAWSCredentialIdentityResolver: AwsCommonRuntimeKit.CredentialsProvider
     /// Creates a credentials provider that sources credentials from ec2 instance metadata.
     /// It will use IMDSv2 to fetch the credentials.
     public init() throws {
-        self.crtCredentialsProvider = try CRTCredentialsProvider(source: .imds(
-            bootstrap: SDKDefaultIO.shared.clientBootstrap
-        ))
+        self.crtAWSCredentialIdentityResolver = try AwsCommonRuntimeKit.CredentialsProvider(
+            source: .imds(
+                bootstrap: SDKDefaultIO.shared.clientBootstrap
+            )
+        )
     }
 }
