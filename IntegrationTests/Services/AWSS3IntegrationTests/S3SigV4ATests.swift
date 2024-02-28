43a691149256170859c0493ce3254839d68f5d0e
Sichan Yoo <chanyoo@amazon.com>
2024-02-28 14:09:07 -0800

    Improve AWSS3 SigV4A integration tests that use MRAP.

--- IntegrationTests/Services/AWSS3IntegrationTests/S3SigV4ATests.swift
+++ IntegrationTests/Services/AWSS3IntegrationTests/S3SigV4ATests.swift
@@ -27,25 +27,28 @@ class S3SigV4ATests: S3XCTestCase {
     private var mrapArnFormat = "arn:aws:s3::%@:accesspoint/%@"
     private var mrapArn: String!
     private var mrapAlias: String!
+    private let mrapNamePrefix = "aws-sdk-s3-integration-test-"
     private let mrapName = "aws-sdk-s3-integration-test-" + UUID().uuidString.split(separator: "-").first!.lowercased()
+    private var mrapConfig: S3ControlClientTypes.CreateMultiRegionAccessPointInput!
 
     // The S3 control client used to create and delete MRAP
     private var s3ControlClient: S3ControlClient!
-    
+
     // The STS client for fetching AWS account ID
     private var stsClient: STSClient!
     private var accountId: String!
 
     // Key string used for putting object in tests
-    private let key = "text.txt"
+    private let key = UUID().uuidString.split(separator: "-").first!.lowercased()
 
     private let NSEC_PER_SEC = 1_000_000_000
 
-    override func setUp() async throws {
-        // Create a bucket
-        try await super.setUp()
+    // MARK: - SETUP & TEARDOWN
 
-        // Create sigv4a-only S3 client
+    override func setUp() async throws {
+        s3ControlClient = try S3ControlClient(region: region)
+
+        // Create sigv4a-only S3 client to be used for tests.
         sigv4aConfig = try await S3Client.S3ClientConfiguration(region: region)
         sigv4aConfig.authSchemes = [SigV4AAuthScheme()]
         sigv4aClient = S3Client(config: sigv4aConfig)
@@ -54,84 +57,28 @@ class S3SigV4ATests: S3XCTestCase {
         stsClient = try STSClient(region: region)
         accountId = try await stsClient.getCallerIdentity(input: GetCallerIdentityInput()).account
 
-        // Construct MRAP config
-        let publicAccessBlock = S3ControlClientTypes.PublicAccessBlockConfiguration(
-            blockPublicAcls: false,
-            blockPublicPolicy: false,
-            ignorePublicAcls: false,
-            restrictPublicBuckets: false
-        )
-        let regions = [S3ControlClientTypes.Region(bucket: bucketName, bucketAccountId: accountId)]
-        let mrapConfig = S3ControlClientTypes.CreateMultiRegionAccessPointInput(
-            name: mrapName,
-            publicAccessBlock: publicAccessBlock,
-            regions: regions
-        )
-
-        // Create S3 Multi-Region Access Point (MRAP)
-        s3ControlClient = try S3ControlClient(region: region)
-        let createMRAPInput = CreateMultiRegionAccessPointInput(
-            accountId: accountId,
-            clientToken: UUID().uuidString.split(separator: "-").first!.lowercased(),
-            details: mrapConfig
-        )
-        _ = try await s3ControlClient.createMultiRegionAccessPoint(input: createMRAPInput).requestTokenARN
-
-        // Wait until MRAP creation finishes
-        var status: S3ControlClientTypes.MultiRegionAccessPointStatus? = .creating
-        repeat {
-            let seconds = 20.0
-            try await Task.sleep(nanoseconds: UInt64(seconds * Double(NSEC_PER_SEC)))
-
-            status = try await s3ControlClient.getMultiRegionAccessPoint(input: GetMultiRegionAccessPointInput(
-                accountId: accountId,
-                name: mrapName
-            )).accessPoint?.status
-        } while status == .creating
-
-        // Fetch MRAP alias then format & save MRAP ARN
-        // This MRAP ARN is used in place of bucket name in subsequent calls
-        mrapAlias = try await s3ControlClient.getMultiRegionAccessPoint(input: GetMultiRegionAccessPointInput(
-            accountId: accountId,
-            name: mrapName
-        )).accessPoint?.alias
-        mrapArn = String(format: mrapArnFormat, accountId, mrapAlias)
+        // Save mrapArn without creating new MRAP if it already exists.
+        if let preexistingMRAP = try await checkIfMRAPExists(), let alias = preexistingMRAP.alias {
+            mrapArn = String(format: mrapArnFormat, accountId, alias)
+        } else {
+            // Otherwise, create a new MRAP.
+            // Create a bucket to be linked to MRAP at construction.
+            try await super.setUp()
+            // Construct MRAP and save its ARN for use in tests.
+            _ = try await constructMRAP()
+        }
     }
 
     override func tearDown() async throws {
-        // MRAP must be deleted before deleting the linked bucket
-        try await deleteMRAP()
-        // Empty & delete the bucket
-        try await super.tearDown()
+        /*
+         * Intentional No-Op:
+         * Note that MRAP is not deleted to be re-used in subsequent test runs.
+         * And because MRAP is not deleted, the bucket associated with it cannot
+         * be deleted either, hence the override with no-op.
+         */
     }
 
-    private func deleteMRAP() async throws {
-        // Delete the multi-region access point
-        _ = try await s3ControlClient.deleteMultiRegionAccessPoint(input: DeleteMultiRegionAccessPointInput(
-            accountId: accountId,
-            clientToken: UUID().uuidString.split(separator: "-").first!.lowercased(),
-            details: S3ControlClientTypes.DeleteMultiRegionAccessPointInput(name: mrapName)
-        ))
-
-        // Wait until MRAP has been deleted before returning
-        var mrapExists = true
-        repeat {
-            let seconds = 20.0
-            try await Task.sleep(nanoseconds: UInt64(seconds * Double(NSEC_PER_SEC)))
-
-            let mraps = try await s3ControlClient.listMultiRegionAccessPoints(input: ListMultiRegionAccessPointsInput(accountId: accountId)).accessPoints
-            mrapExists = checkMRAPExists(mraps ?? [])
-        } while mrapExists
-
-        // Wait some more before returning because
-        // deleting access point association with buckets takes more time
-        let seconds = 60.0
-        try await Task.sleep(nanoseconds: UInt64(seconds * Double(NSEC_PER_SEC)))
-    }
-
-    private func checkMRAPExists(_ mraps: [S3ControlClientTypes.MultiRegionAccessPointReport]) -> Bool {
-        return mraps.contains { $0.name == mrapName }
-    }
+    // MARK: - TEST CASES
 
     func testS3MRAPSigV4A() async throws {
         // Put an object to bucket via MRAP using SigV4A-only client
@@ -198,5 +145,60 @@ class S3SigV4ATests: S3XCTestCase {
             input: ListObjectsV2Input(bucket: mrapArn)
         )
         XCTAssertEqual(response.keyCount, 1)
+    }
+
+    // MARK: - HELPER FUNCTIONS
+
+    // Check if MRAP exists by checking names of existing MRAPs against integ-test MRAP prefix.
+    private func checkIfMRAPExists() async throws -> S3ControlClientTypes.MultiRegionAccessPointReport? {
+        let mraps = try await s3ControlClient.listMultiRegionAccessPoints(input: ListMultiRegionAccessPointsInput(accountId: accountId)).accessPoints
+        return mraps?.first { $0.name?.hasPrefix(mrapNamePrefix) ?? false }
+    }
+
+    private func constructMRAPConfig() {
+        let publicAccessBlock = S3ControlClientTypes.PublicAccessBlockConfiguration(
+            blockPublicAcls: false,
+            blockPublicPolicy: false,
+            ignorePublicAcls: false,
+            restrictPublicBuckets: false
+        )
+        let regions = [S3ControlClientTypes.Region(bucket: bucketName, bucketAccountId: accountId)]
+        mrapConfig = S3ControlClientTypes.CreateMultiRegionAccessPointInput(
+            name: mrapName,
+            publicAccessBlock: publicAccessBlock,
+            regions: regions
+        )
+    }
+
+    private func constructMRAP() async throws {
+        // Construct MRAP config to use
+        constructMRAPConfig()
+        // Create S3 Multi-Region Access Point (MRAP)
+        let createMRAPInput = CreateMultiRegionAccessPointInput(
+            accountId: accountId,
+            clientToken: UUID().uuidString.split(separator: "-").first!.lowercased(),
+            details: mrapConfig
+        )
+        _ = try await s3ControlClient.createMultiRegionAccessPoint(input: createMRAPInput)
+
+        // Wait until MRAP creation finishes
+        var status: S3ControlClientTypes.MultiRegionAccessPointStatus? = .creating
+        repeat {
+            let seconds = 20.0
+            try await Task.sleep(nanoseconds: UInt64(seconds * Double(NSEC_PER_SEC)))
+
+            status = try await s3ControlClient.getMultiRegionAccessPoint(input: GetMultiRegionAccessPointInput(
+                accountId: accountId,
+                name: mrapName
+            )).accessPoint?.status
+        } while status == .creating
+
+        // Fetch MRAP alias then format & save MRAP ARN
+        // This MRAP ARN is used in place of bucket name in subsequent calls
+        mrapAlias = try await s3ControlClient.getMultiRegionAccessPoint(input: GetMultiRegionAccessPointInput(
+            accountId: accountId,
+            name: mrapName
+        )).accessPoint?.alias
+        mrapArn = String(format: mrapArnFormat, accountId, mrapAlias)
     }
 }
