8f39040ae2bd91f0c763464c5b2ab5d84a0ecd31
Josh Elkins <jbelkins@amazon.com>
2024-04-22 17:55:12 -0500

    Code cleanup

--- codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/protocols/restxml/RestXMLCustomizations.kt
+++ codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/protocols/restxml/RestXMLCustomizations.kt
@@ -10,18 +10,16 @@ import software.amazon.smithy.aws.swift.codegen.AWSHTTPProtocolCustomizations
 import software.amazon.smithy.codegen.core.Symbol
 import software.amazon.smithy.model.shapes.ShapeId
 import software.amazon.smithy.swift.codegen.SwiftWriter
+import software.amazon.smithy.swift.codegen.integration.HTTPProtocolCustomizable.ServiceErrorCustomRenderer
 import software.amazon.smithy.swift.codegen.integration.ProtocolGenerator
 
 class RestXMLCustomizations : AWSHTTPProtocolCustomizations() {
 
     override val baseErrorSymbol: Symbol = AWSClientRuntimeTypes.RestXML.RestXMLError
 
-    override fun hasServiceErrorCustomizations(ctx: ProtocolGenerator.GenerationContext): Boolean {
-        return shouldApplyS3ErrorCustomization(ctx)
-    }
+    private class S3Empty404Renderer : ServiceErrorCustomRenderer {
 
-    override fun renderServiceErrorCustomizations(ctx: ProtocolGenerator.GenerationContext, writer: SwiftWriter) {
-        if (shouldApplyS3ErrorCustomization(ctx)) {
+        override fun render(writer: SwiftWriter) {
             writer.openBlock(
                 "if baseError.httpResponse.statusCode == .notFound && baseError.httpResponse.body.isEmpty {",
                 "}"
@@ -29,6 +27,12 @@ class RestXMLCustomizations : AWSHTTPProtocolCustomizations() {
                 writer.write("return try NotFound.makeError(baseError: baseError)")
             }
         }
+    }
+
+    override fun serviceErrorCustomRenderer(
+        ctx: ProtocolGenerator.GenerationContext
+    ): ServiceErrorCustomRenderer? {
+        return S3Empty404Renderer().takeIf { shouldApplyS3ErrorCustomization(ctx) }
     }
 
     private fun shouldApplyS3ErrorCustomization(ctx: ProtocolGenerator.GenerationContext): Boolean {
@@ -39,8 +43,8 @@ class RestXMLCustomizations : AWSHTTPProtocolCustomizations() {
         return ctx.service.id == ShapeId.from("com.amazonaws.s3#AmazonS3")
     }
 
-    // This check is performed because S3 protocol tests do not define this error,
-    // and the protocol test will fail since the NotFound type is undefined.
+    // This check is performed because S3 protocol tests do not define the NotFound modeled error,
+    // and the protocol test will fail if it is undefined.
     private fun serviceHasNotFoundError(ctx: ProtocolGenerator.GenerationContext): Boolean {
         return ctx.model.getShape(ShapeId.from("com.amazonaws.s3#NotFound")).isPresent
     }
