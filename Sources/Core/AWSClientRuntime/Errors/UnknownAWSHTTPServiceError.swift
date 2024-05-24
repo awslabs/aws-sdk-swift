df84104a163d6b4f568544d50ebb2c8da1f72875
Sichan Yoo <chanyoo@amazon.com>
2024-05-24 11:29:25 -0700

    Make UnknownAWSHTTPServiceError internal with @_spi(Internal) tag.

--- Sources/Core/AWSClientRuntime/Errors/UnknownAWSHTTPServiceError.swift
+++ Sources/Core/AWSClientRuntime/Errors/UnknownAWSHTTPServiceError.swift
@@ -6,7 +6,8 @@
 import ClientRuntime
 
 /// AWS specific Service Error structure used when exact error could not be deduced from the `HttpResponse`
-public struct UnknownAWSHTTPServiceError: AWSServiceError, HTTPError, Error {
+/// Developers should catch unknown errors by the interface `AWSServiceError`, then use the `errorCode` to determine & handle each type of error.
+@_spi(Internal) public struct UnknownAWSHTTPServiceError: AWSServiceError, HTTPError, Error {
 
     public var errorCode: String? { typeName }
 
