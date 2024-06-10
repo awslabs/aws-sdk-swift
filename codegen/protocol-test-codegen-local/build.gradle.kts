63e3dbfc57cab19d6dd97fdb0a18ef2e55416902
Sichan Yoo <chanyoo@amazon.com>
2024-06-10 13:47:00 -0700

    Bump Swift from 5.7 to 5.9 & bump simulator versions as well

--- codegen/protocol-test-codegen-local/build.gradle.kts
+++ codegen/protocol-test-codegen-local/build.gradle.kts
@@ -62,7 +62,7 @@ fun generateSmithyBuild(tests: List<CodegenTest>): String {
                       "gitRepo": "https://github.com/aws-amplify/smithy-swift.git",
                       "author": "Amazon Web Services",
                       "homepage": "https://docs.amplify.aws/",
-                      "swiftVersion": "5.7.0",
+                      "swiftVersion": "5.9.0",
                       "build": {
                         "rootProject": true
                       },
