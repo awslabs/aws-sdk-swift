4593a214b13acf287d2725c417592920eb409476
Josh Elkins <jbelkins@users.noreply.github.com>
2024-07-11 14:03:41 -0500

    fix: Add needed event streams auth test dependency (#1625)

--- Package.swift
+++ Package.swift
@@ -121,7 +121,7 @@ let package = Package(
         ),
         .testTarget(
             name: "AWSSDKEventStreamsAuthTests",
-            dependencies: ["AWSClientRuntime", "AWSSDKEventStreamsAuth", .smithyStreams],
+            dependencies: ["AWSClientRuntime", "AWSSDKEventStreamsAuth", .smithyStreams, .smithyTestUtils],
             path: "Sources/Core/AWSSDKEventStreamsAuth/Tests/AWSSDKEventStreamsAuthTests"
         ),
         .testTarget(
