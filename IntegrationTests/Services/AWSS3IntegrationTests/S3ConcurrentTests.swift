fed7e456c22ccb6c786318af5cfc874659a1f3f3
Sichan Yoo <chanyoo@amazon.com>
2024-09-20 15:16:08 -0700

    Reduce payload from 5MB to 3MB

--- IntegrationTests/Services/AWSS3IntegrationTests/S3ConcurrentTests.swift
+++ IntegrationTests/Services/AWSS3IntegrationTests/S3ConcurrentTests.swift
@@ -21,24 +21,24 @@ class S3ConcurrentTests: S3XCTestCase {
     }
 
     // Run test 100 times, concurrently
-    func test_100x_5MB_getObject() async throws {
-        try await repeatConcurrentlyWithArgs(count: 100, test: getObject_5MB, args: fileData!)
+    func test_100x_3MB_getObject() async throws {
+        try await repeatConcurrentlyWithArgs(count: 100, test: getObject_3MB, args: fileData!)
     }
 
     /* Helper functions */
 
-    // Generates 5MB text data
+    // Generates 3MB text data
     func generateDummyTextData() throws -> Data {
         let segmentData = Data("1234567890abcdefghijklmnopqrstABCDEFGHIJKLMNOPQRST".utf8)
         var wholeData = Data()
-        for _ in 0..<100_000 {
+        for _ in 0..<60_000 {
             wholeData.append(segmentData)
         }
         return wholeData
     }
 
-    // Puts 5MB data to S3, gets the uploaded file, then asserts retrieved data equals original data
-    func getObject_5MB(args: Any...) async throws {
+    // Puts 3MB data to S3, gets the uploaded file, then asserts retrieved data equals original data
+    func getObject_3MB(args: Any...) async throws {
         guard let data = args[0] as? Data else {
             throw ClientError.dataNotFound("Failed to retrieve dummy data.")
         }
