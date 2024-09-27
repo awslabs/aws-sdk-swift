e117102522aa9cf6cdd0e8b2aecd14fb6b12f6f1
Sichan Yoo <chanyoo@amazon.com>
2024-09-27 15:43:43 -0700

    Add service feature & service documentation sections to release notes generation. Also, refactor it for consistent spacing between the lines even when sections are omitted.

--- AWSSDKSwiftCLI/Sources/AWSSDKSwiftCLI/Models/ReleaseNotesBuilder.swift
+++ AWSSDKSwiftCLI/Sources/AWSSDKSwiftCLI/Models/ReleaseNotesBuilder.swift
@@ -18,21 +18,60 @@ struct ReleaseNotesBuilder {
     
     // MARK: - Build
     
-    func build() -> String {
-        let contents = [
-            "## What's Changed",
-            buildCommits(),
-            .newline,
-            "**Full Changelog**: https://github.com/\(repoOrg.rawValue)/\(repoType.rawValue)/compare/\(previousVersion)...\(newVersion)"
-        ]
+    func build() throws -> String {
+        let contents: [String] = (
+            ["## What's Changed"] +
+            buildSDKChangeSection() +
+            (try buildServiceChangeSection()) +
+            ["\n**Full Changelog**: https://github.com/\(repoOrg.rawValue)/\(repoType.rawValue)/compare/\(previousVersion)...\(newVersion)"]
+        )
         return contents.joined(separator: .newline)
     }
-    
-    // Adds a preceding `*` to each commit string
-    // This renders the list of commits as a list in markdown
-    func buildCommits() -> String {
-        commits
-            .map { "* \($0)"}
+
+    func buildSDKChangeSection() -> [String] {
+        let formattedCommits = commits
+            .filter { !$0.hasPrefix("chore") && !$0.hasPrefix("Update") }
+            .map { "* \($0)" }
             .joined(separator: .newline)
+        if (!formattedCommits.isEmpty) {
+            return ["### AWS SDK for Swift", formattedCommits]
+        }
+        return []
+    }
+
+    func buildServiceChangeSection() throws -> [String] {
+        var features: Features
+        var mapping: [String: String]
+        features = try Features.fromFile("build-request.json")
+        mapping = try Features.mappingFromFile("feature-service-id.json")
+        return buildServiceFeatureSection(features, mapping) + buildServiceDocSection(features, mapping)
+    }
+
+    private func buildServiceFeatureSection(
+        _ features: Features,
+        _ mapping: [String: String]
+    ) -> [String] {
+        let formattedFeatures = features.features
+            .filter { $0.featureType == "NEW_FEATURE" }
+            .map { "* **AWS \(mapping[$0.featureId]!)**: \($0.releaseNotes)" }
+            .joined(separator: .newline)
+        if (!formattedFeatures.isEmpty) {
+            return ["### Service Features", formattedFeatures]
+        }
+        return []
+    }
+
+    private func buildServiceDocSection(
+        _ features: Features,
+        _ mapping: [String: String]
+    ) -> [String] {
+        let formattedDocUpdates = features.features
+            .filter { $0.featureType == "DOC_UPDATE" }
+            .map { "* **AWS \(mapping[$0.featureId]!)**: \($0.releaseNotes)" }
+            .joined(separator: .newline)
+        if (!formattedDocUpdates.isEmpty) {
+            return ["### Service Documentation", formattedDocUpdates]
+        }
+        return []
     }
 }
