98bded01883e44dc4109549c080c52b78250ce70
Sichan Yoo <chanyoo@amazon.com>
2025-01-03 14:52:11 -0800

    Add the ignoreConfiguredEndpointURLs config field in runtime side.

--- Sources/Core/AWSClientRuntime/Sources/AWSClientRuntime/Config/AWSDefaultClientConfiguration.swift
+++ Sources/Core/AWSClientRuntime/Sources/AWSClientRuntime/Config/AWSDefaultClientConfiguration.swift
@@ -45,4 +45,31 @@ public protocol AWSDefaultClientConfiguration {
     ///
     /// If set, this value gets used when resolving max attempts value from the standard progression of potential sources. If no value could be resolved, the SDK uses max attempts value of 3 by default.
     var maxAttempts: Int? { get set }
+
+    /// Specifies whether the endpoint configured via environment variable or shared config file should be used by the service client.
+    ///
+    /// If `false`, the endpoint for the service client is resolved in the following order:
+    ///     1. The `endpoint` value provided at service client initialization via the config.
+    ///     2. The value of `AWS_ENDPOINT_URL_<SERVICE>` environment variable where `<SERVICE>` is the transformed value of the given service client's `serviceName` property, where spaces are replaced with underscores and letters are uppercased. E.g., `"API Gateway"` => `"API_GATEWAY"`.
+    ///     3. The value of `AWS_ENDPOINT_URL` environment variable; this is used to configure the endpoint for all services.
+    ///     4. In the shared config file, the `endpoint_url` property under selected profile's `services` section's `<SERVICE> =` subsection. The `<SERVICE>` value is the transformed value of the given service client's `serviceName` property, where spaces are replaced with underscores and letters are lowercased. E.g., `"API Gateway` => `"api_gateway`.
+    ///     5. In the shared config file, the `endpoint_url` property under the selected profile; this is used to configure the endpoint for all services.
+    ///     6. If the endpoint wasn't configured anywhere, then AWS Swift SDK determines the endpoint to use for each operation call by using the built-in endpoint resolver for the service.
+    /// Note: For the profile name used in resolution steps 4 & 5, the value of `AWS_PROFILE` environment variable is used if set. Otherwise, `default` is used.
+    ///
+    /// If `true`, the endpoint for the service client is resolved in the following order:
+    ///     1. The `endpoint` value provided at service client initialization via the config.
+    ///     2. If `endpoint` wasn't configured by the user via service client config, AWS Swift SDK determines the endpoint to use for each operation call by using the built-in endpoint reoslver for the service.
+    ///
+    /// If this flag isn't set, AWS Swift SDK look at the corresponding environment variable and shared config file property when `endpoint` service client config is resolved. The flag value gets dermined by looking at the possible sources in the following order:
+    ///     1. The value of  `ignoreConfiguredEndpointURLs` in the client config.
+    ///     2. The value of `AWS_IGNORE_CONFIGURED_ENDPOINT_URLS` environment variable.
+    ///     3. In the shared config file, the value of `ignore_configured_endpoint_urls` property under the selected profile.
+    ///     4. If this flag was not set anywhere, the AWS Swift SDK attempts to resolve configured endpoint.
+    /// Note: For the profile name used in resolution step 3, the value of`AWS_PROFILE` environment variable is used if set. Otherwise, `default` is used.
+    ///
+    /// For more information on service-specific endpoints, see the [official AWS documentation](https://docs.aws.amazon.com/sdkref/latest/guide/feature-ss-endpoints.html).
+    ///
+    /// For the list of valid `<SERVICE>` values for the services, refer to [the official list](https://docs.aws.amazon.com/sdkref/latest/guide/ss-endpoints-table.html).
+    var ignoreConfiguredEndpointURLs: Bool? { get set }
 }
