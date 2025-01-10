45bc7907e4f56019a2999aa5d4da65fed9cce1e8
Sichan Yoo <chanyoo@amazon.com>
2025-01-10 13:57:19 -0800

    Revert S3TM config to take s3 client instance. No need to add intercpetor for header modification; I can just resolve and input values to input struct, which then gets serialized automatically by header provider.

--- Sources/Core/AWSServiceExtensions/Sources/AWSS3TransferManager/AWSS3TransferManagerConfig.swift
+++ Sources/Core/AWSServiceExtensions/Sources/AWSS3TransferManager/AWSS3TransferManagerConfig.swift
@@ -9,7 +9,7 @@ import AWSS3
 import SmithyChecksumsAPI
 
 public class AWSS3TransferManagerConfig {
-    var s3ClientConfig: S3Client.S3ClientConfiguration
+    var s3Client: S3Client
     var targetPartSizeBytes: Int
     var multipartUploadThresholdBytes: Int
     var checksumValidationEnabled: Bool
@@ -19,24 +19,24 @@ public class AWSS3TransferManagerConfig {
     /// Creates the config object for the AWS S3 Transfer Manager.
     ///
     /// - Parameters:
-    ///    - s3ClientConfig: The S3 client config instance used by the transfer manager. If not provided, a default config is created for use.
+    ///    - s3Client: The S3 client instance to use for the transfer manager. If not provided, a default client is created for use.
     ///    - targetPartSizeBytes: The part size used by multipart operations. The last part can be shorter. Default value is 8MB.
     ///    - multipartUploadThresholdBytes:The threshold at which multipart operations get used instead of single put/get object operations. Default value is 16MB.
     ///    - checksumValidationEnabled: Specifies whether checksum should be validated in download operations. Default value is true.
     ///    - checksumAlgorithm: Specifies the checksum algorithm to use for upload operations. Default algorithm is CRC32.
     ///    - multipartDownloadTypes: Specifies the behavior of getObject for multipart download operations. Default value is `.part`, which configures getObject to use part numbers for multipart download. The other option is `.range`, which uses the actual byte range of the S3 object to download. If what you want to download was uploaded without using MPU (therefore there's no part number available), then you must use `.range`.
     public init(
-        s3ClientConfig: S3Client.S3ClientConfiguration? = nil,
+        s3Client: S3Client? = nil,
         targetPartSizeBytes: Int = 8_000_000,
         multipartUploadThresholdBytes: Int = 16_000_000,
         checksumValidationEnabled: Bool = true,
         checksumAlgorithm: SmithyChecksumsAPI.ChecksumAlgorithm = .crc32,
         multipartDownloadType: MultipartyDownloadType = .part
     ) async throws {
-        if let s3ClientConfig {
-            self.s3ClientConfig = s3ClientConfig
+        if let s3Client {
+            self.s3Client = s3Client
         } else {
-            self.s3ClientConfig = try await S3Client.S3ClientConfiguration()
+            self.s3Client = try await S3Client()
         }
         self.targetPartSizeBytes = targetPartSizeBytes
         self.multipartUploadThresholdBytes = multipartUploadThresholdBytes
