57c8b1e2e2dee6b79b048629aeccb551ccee1198
Josh Elkins <jbelkins@amazon.com>
2025-01-14 14:51:26 -0600

    Add logging for early exit

--- AWSSDKSwiftCLI/Sources/AWSSDKSwiftCLI/Commands/AWSSDKSwiftCLI/Subcommands/PrepareRelease.swift
+++ AWSSDKSwiftCLI/Sources/AWSSDKSwiftCLI/Commands/AWSSDKSwiftCLI/Subcommands/PrepareRelease.swift
@@ -82,11 +82,20 @@ struct PrepareRelease {
         
         let previousVersion = try getPreviousVersion()
         guard try repoHasChanges(previousVersion) && FeaturesReader.buildRequestAndMappingExist() else {
-            /// If repo has no changes or if the build request or mapping input files
-            /// don't exist, create an empty release-manifest.json file.
+            /// If repo has no changes, create an empty release-manifest.json file.
             /// Empty manifest file makes GitHubReleasePublisher be no-op.
             /// The manifest file is required regardless of whether there should
             /// be a release or not.
+            log("Repo has no changes, writing empty manifest and exiting.")
+            try createEmptyReleaseManifest()
+            /// Return without creating new commit or tag in local repos.
+            /// This makes GitPublisher be no-op.
+            return
+        }
+        guard FeaturesReader.buildRequestAndMappingExist() else {
+            /// If the build request or mapping input files
+            /// don't exist, create an empty release-manifest.json file.
+            log("build-request.json and/or feature-service-id.json don't exist, writing empty manifest and exiting.")
             try createEmptyReleaseManifest()
             /// Return without creating new commit or tag in local repos.
             /// This makes GitPublisher be no-op.
