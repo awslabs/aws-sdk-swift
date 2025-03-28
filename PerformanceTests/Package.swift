2dbf482f2254816f80653bcfa927a50588e1d419
David Yaffe <dayaffe@amazon.com>
2025-03-28 12:30:06 -0400

    feat: Add PerformanceTests (#1910)

---
+++ PerformanceTests/Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "SDKWorkbench",
+    platforms: [.macOS(.v12), .iOS(.v15)],
+    dependencies: [
+        .package(name: "aws-sdk-swift", path: ".."),
+    ],
+    targets: [
+        .executableTarget(
+            name: "PerformanceTestRunner",
+            dependencies: [
+                .product(name: "AWSSTS", package: "aws-sdk-swift"),
+            ]
+        ),
+    ]
+)
---
+++ PerformanceTests/Sources/PerformanceTestRunner/Core/Runners/ProcessRunner.swift
@@ -0,0 +1,44 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import Foundation
+
+struct ProcessRunner {
+    static func runProcess(
+        executable: String = "/usr/bin/env",
+        arguments: [String]
+    ) -> String {
+        let process = Process()
+        process.executableURL = URL(fileURLWithPath: executable)
+        process.arguments = arguments
+
+        let outputPipe = Pipe()
+        process.standardOutput = outputPipe
+        process.standardError = Pipe()
+
+        do {
+            try process.run()
+            process.waitUntilExit()
+            let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+            return String(data: outputData, encoding: .utf8)?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+        } catch {
+            print("ProcessRunner Error: \(error.localizedDescription)")
+            return ""
+        }
+    }
+
+    /// Retrieves the current Git commit ID.
+    static func getGitCommitId() -> String {
+        return ProcessRunner.runProcess(arguments: ["git", "rev-parse", "HEAD"])
+    }
+
+    /// Retrieves the SDK version from a file.
+    static func getSdkVersion() -> String {
+        let version = ProcessRunner.runProcess(arguments: ["cat", "../../aws-sdk-swift/Package.version"])
+        return version.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+}
