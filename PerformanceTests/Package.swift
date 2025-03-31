43bb0fc9a3c356e77ca4789212ba86edebbc2432
David Yaffe <dayaffe@amazon.com>
2025-03-31 17:08:00 -0400

    add executable as product (#1912)

--- PerformanceTests/Package.swift
+++ PerformanceTests/Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 let package = Package(
     name: "SDKWorkbench",
     platforms: [.macOS(.v12), .iOS(.v15)],
+    products: [
+        .executable(
+            name: "PerformanceTestRunner",
+            targets: ["PerformanceTestRunner"]
+        )
+    ],
     dependencies: [
         .package(name: "aws-sdk-swift", path: ".."),
     ],
