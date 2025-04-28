da01952f6f8d12b7ad9811c713c9afbf17b9d8b9
Sichan Yoo <chanyoo@amazon.com>
2025-04-28 12:12:16 -0700

    Decompose CRT default chain into individual wrappers.

--- Sources/Core/AWSSDKIdentity/Sources/AWSSDKIdentity/AWSCredentialIdentityResolvers/DefaultAWSCredentialIdentityResolverChain.swift
+++ Sources/Core/AWSSDKIdentity/Sources/AWSSDKIdentity/AWSCredentialIdentityResolvers/DefaultAWSCredentialIdentityResolverChain.swift
@@ -9,6 +9,8 @@ import class AwsCommonRuntimeKit.CredentialsProvider
 import ClientRuntime
 import protocol SmithyIdentity.AWSCredentialIdentityResolvedByCRT
 @_spi(FileBasedConfig) import AWSSDKCommon
+import protocol SmithyIdentity.AWSCredentialIdentityResolver
+import struct Smithy.Attributes
 
 // swiftlint:disable type_name
 // ^ Required to mute swiftlint warning about type name being too long.
@@ -24,21 +26,33 @@ import protocol SmithyIdentity.AWSCredentialIdentityResolvedByCRT
 /// 5. EC2 Instance Metadata (IMDSv2)
 ///
 /// The credentials retrieved from the chain are cached for 15 minutes.
-public struct DefaultAWSCredentialIdentityResolverChain: AWSCredentialIdentityResolvedByCRT {
-    public let crtAWSCredentialIdentityResolver: AwsCommonRuntimeKit.CredentialsProvider
-
+public struct DefaultAWSCredentialIdentityResolverChain: AWSCredentialIdentityResolver {
     /// Creates a credential identity resolver that uses the default AWS credential identity resolver chain used by most AWS SDKs.
-    public init() throws {
-        let fileBasedConfig = try CRTFileBasedConfiguration()
-        try self.init(fileBasedConfig: fileBasedConfig)
-    }
+    public init() {}
 
-    @_spi(DefaultAWSCredentialIdentityResolverChain)
-    public init(fileBasedConfig: CRTFileBasedConfiguration) throws {
-        self.crtAWSCredentialIdentityResolver = try AwsCommonRuntimeKit.CredentialsProvider(source: .defaultChain(
-            bootstrap: SDKDefaultIO.shared.clientBootstrap,
-            fileBasedConfiguration: fileBasedConfig
-        ))
+    public func getIdentity(identityProperties: Attributes?) async throws -> AWSCredentialIdentity {
+        typealias ResolverFactory = () throws -> any AWSCredentialIdentityResolver
+
+        let resolverFactories: [ResolverFactory] = [
+            { try EnvironmentAWSCredentialIdentityResolver() },
+            { try ProfileAWSCredentialIdentityResolver() },
+            { try STSWebIdentityAWSCredentialIdentityResolver() },
+            { try ECSAWSCredentialIdentityResolver() },
+            { try IMDSAWSCredentialIdentityResolver() }
+        ]
+
+        let lastIndex = resolverFactories.count - 1
+        for index in 0..<lastIndex {
+            do {
+                let resolver = try resolverFactories[index]()
+                return try await resolver.getIdentity(identityProperties: identityProperties)
+            } catch {
+                // Continue to the next resolver factory.
+            }
+        }
+
+        // The error thrown from the last resolver is not caught and instead gets thrown to caller.
+        return try await resolverFactories[lastIndex]().getIdentity(identityProperties: identityProperties)
     }
 }
 
