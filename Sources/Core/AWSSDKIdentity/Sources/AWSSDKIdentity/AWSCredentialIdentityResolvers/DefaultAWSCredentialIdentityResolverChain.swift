075cc5ca009834b24dc96aee4ab1326c04cde106
Sichan Yoo <chanyoo@amazon.com>
2025-05-09 09:51:35 -0700

    Add caching to decomposed default chain.

--- Sources/Core/AWSSDKIdentity/Sources/AWSSDKIdentity/AWSCredentialIdentityResolvers/DefaultAWSCredentialIdentityResolverChain.swift
+++ Sources/Core/AWSSDKIdentity/Sources/AWSSDKIdentity/AWSCredentialIdentityResolvers/DefaultAWSCredentialIdentityResolverChain.swift
@@ -26,33 +26,57 @@ import struct Smithy.Attributes
 /// 5. EC2 Instance Metadata (IMDSv2)
 ///
 /// The credentials retrieved from the chain are cached for 15 minutes.
-public struct DefaultAWSCredentialIdentityResolverChain: AWSCredentialIdentityResolver {
+public class DefaultAWSCredentialIdentityResolverChain: AWSCredentialIdentityResolver {
     /// Creates a credential identity resolver that uses the default AWS credential identity resolver chain used by most AWS SDKs.
-    public init() {}
+    typealias ResolverFactory = () throws -> (any AWSCredentialIdentityResolver)
+
+    private let resolverFactories: [ResolverFactory]
+    private var cachedCredentials: AWSCredentialIdentity? = nil
+
+    public init() {
+        resolverFactories = [
+            { return (try EnvironmentAWSCredentialIdentityResolver()) },
+            { return (try ProfileAWSCredentialIdentityResolver()) },
+            { return (try STSWebIdentityAWSCredentialIdentityResolver()) },
+            { return (try ECSAWSCredentialIdentityResolver()) },
+            { return (try IMDSAWSCredentialIdentityResolver()) }
+        ]
+    }
 
     public func getIdentity(identityProperties: Attributes?) async throws -> AWSCredentialIdentity {
-        typealias ResolverFactory = () throws -> any AWSCredentialIdentityResolver
-
-        let resolverFactories: [ResolverFactory] = [
-            { try EnvironmentAWSCredentialIdentityResolver() },
-            { try ProfileAWSCredentialIdentityResolver() },
-            { try STSWebIdentityAWSCredentialIdentityResolver() },
-            { try ECSAWSCredentialIdentityResolver() },
-            { try IMDSAWSCredentialIdentityResolver() }
-        ]
+        if let cached = cachedCredentials, !shouldRefreshCredentials(expiration: cached.expiration) {
+            return cached
+        }
 
         let lastIndex = resolverFactories.count - 1
         for index in 0..<lastIndex {
             do {
                 let resolver = try resolverFactories[index]()
-                return try await resolver.getIdentity(identityProperties: identityProperties)
+                let credentials = try await resolver.getIdentity(identityProperties: identityProperties)
+                cachedCredentials = credentials
+                return credentials
             } catch {
                 // Continue to the next resolver factory.
             }
         }
 
         // The error thrown from the last resolver is not caught and instead gets thrown to caller.
-        return try await resolverFactories[lastIndex]().getIdentity(identityProperties: identityProperties)
+        let lastResolver = try resolverFactories[lastIndex]()
+        let credentials = try await lastResolver.getIdentity(identityProperties: identityProperties)
+        cachedCredentials = credentials
+        return credentials
+    }
+
+    private func shouldRefreshCredentials(expiration: Date?) -> Bool {
+        guard let expiration = expiration else {
+            return false // No expiration provided, assume no refresh is needed
+        }
+
+        let now = Date()
+        let tenMinutesBeforeExpiration = expiration.addingTimeInterval(-600)
+
+        // Return true if now is after (expiration - 10 minutes).
+        return now >= tenMinutesBeforeExpiration
     }
 }
 
