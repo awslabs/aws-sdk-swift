0f0af49b386715b36e4ecf6e178a51b03cf9f750
Sichan Yoo <chanyoo@amazon.com>
2025-06-13 10:59:17 -0700

    Add codegen for IdentityProvidingSSOClient and IdentityProvidingSSOOIDCClient structs.

--- codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/customization/credentialresolverservices/IdentityProvidingSSOOIDCClientIntegration.kt
+++ codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/customization/credentialresolverservices/IdentityProvidingSSOOIDCClientIntegration.kt
@@ -1,11 +1,14 @@
 package software.amazon.smithy.aws.swift.codegen.customization.credentialresolverservices
 
+import software.amazon.smithy.aws.swift.codegen.swiftmodules.AWSSDKIdentityTypes
 import software.amazon.smithy.model.Model
 import software.amazon.smithy.swift.codegen.SwiftDelegator
 import software.amazon.smithy.swift.codegen.SwiftSettings
 import software.amazon.smithy.swift.codegen.core.SwiftCodegenContext
 import software.amazon.smithy.swift.codegen.integration.ProtocolGenerator
 import software.amazon.smithy.swift.codegen.integration.SwiftIntegration
+import software.amazon.smithy.swift.codegen.swiftmodules.FoundationTypes
+import software.amazon.smithy.swift.codegen.swiftmodules.SmithyIdentityTypes
 
 class IdentityProvidingSSOOIDCClientIntegration : SwiftIntegration {
     override fun enabledForService(
@@ -18,6 +21,31 @@ class IdentityProvidingSSOOIDCClientIntegration : SwiftIntegration {
         protocolGenerationContext: ProtocolGenerator.GenerationContext,
         delegator: SwiftDelegator,
     ) {
-
+        val path = "Sources/InternalAWSSSOOIDC/IdentityProvidingSSOOIDCClient.swift"
+        delegator.useFileWriter(path) { writer ->
+            writer.openBlock(
+                "package struct IdentityProvidingSSOOIDCClient: \$N {",
+                "}",
+                AWSSDKIdentityTypes.IdentityProvidingSSOOIDCClient,
+            ) {
+                writer.write("package init() {}")
+                writer.openBlock(
+                    "package func createToken(region: String, clientID: String, clientSecret: String, refreshToken: String) async throws -> (refreshToken: String?, \$N) {",
+                    "}",
+                    SmithyIdentityTypes.BearerTokenIdentity,
+                ) {
+                    writer.write("let ssoOIDC = try SSOOIDCClient(region: region)")
+                    writer.write("let input = CreateTokenInput(clientId: clientID, clientSecret: clientSecret, grantType: \"refresh_token\", refreshToken: refreshToken)")
+                    writer.write("let out = try await ssoOIDC.createToken(input: input)")
+                    writer.write(
+                        "let expiration = \$N().addingTimeInterval(\$N(out.expiresIn))",
+                        FoundationTypes.Date,
+                        FoundationTypes.TimeInterval,
+                    )
+                    writer.write("let token = BearerTokenIdentity(token: out.accessToken ?? \"\", expiration: expiration)")
+                    writer.write("return (out.refreshToken, token)")
+                }
+            }
+        }
     }
 }
