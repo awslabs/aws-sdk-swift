9c39304451ad8543131ac31229b39fe97978b11e
Josh Elkins <jbelkins@users.noreply.github.com>
2025-06-23 16:26:30 -0500

    feat: S3 Express support (#1906)

---
+++ Sources/Core/AWSSDKIdentity/Sources/AWSSDKIdentity/S3Express/S3ExpressIdentityResolver.swift
@@ -0,0 +1,11 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import struct Smithy.Attributes
+import protocol SmithyIdentityAPI.IdentityResolver
+
+public protocol S3ExpressIdentityResolver: IdentityResolver where IdentityT == S3ExpressIdentity {}
