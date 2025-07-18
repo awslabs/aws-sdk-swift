5ded33415026209ccb2d8a0ff16e057f1110d522
Josh Elkins <jbelkins@amazon.com>
2025-07-18 17:15:21 -0500

    Add tests for config file sections

--- Tests/ConfigFileReaderTests/ConfigFileReaderTests.swift
+++ Tests/ConfigFileReaderTests/ConfigFileReaderTests.swift
@@ -13,6 +13,43 @@ import Foundation
 final class ConfigFileReaderTests: XCTestCase {
     let exampleConfigFilePath = Bundle.module.path(forResource: "config_file_reader_tests", ofType: nil)!
 
+    func test_readsConfigFileDefaultSection() async throws {
+        // arrange
+        let subject = try await TestSubject.constructor(exampleConfigFilePath, nil)
+
+        // act
+        let defaultSection = subject?.section(for: "default")
+
+        // assert
+        XCTAssertNotNil(defaultSection)
+    }
+
+    func test_readsConfigFileBusinessCasualSection() async throws {
+        // arrange
+        let subject = try await TestSubject.constructor(exampleConfigFilePath, nil)
+
+        // act
+        let businessCasualSection = subject?.section(for: "business-casual")
+
+        // assert
+        XCTAssertNotNil(businessCasualSection)
+    }
+
+    func test_configFileHasAllExpectedSections() async throws {
+        // arrange
+        let subject = try await TestSubject.constructor(exampleConfigFilePath, nil)
+
+        // act
+        let sections = [
+            subject?.section(for: "default"),
+            subject?.section(for: "business-casual")
+        ].compactMap { $0 }
+
+        // assert
+        XCTAssertTrue(sections.count == 2)
+    }
+
+
     func test_readsDefaultSection() async throws {
         // arrange
         let subject = try await TestSubject.constructor(exampleConfigFilePath, nil)
