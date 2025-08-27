3ddcb50d6d67b542c80b4a0e929b1802fd6d236d
Josh Elkins <jbelkins@amazon.com>
2025-08-27 10:47:28 -0500

    chore: Update to CRT 0.54.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ import PackageDescription
 // MARK: - Dynamic Content
 
 let clientRuntimeVersion: Version = "0.153.0"
-let crtVersion: Version = "0.52.1"
+let crtVersion: Version = "0.54.0"
 
 let excludeRuntimeUnitTests = false
 
