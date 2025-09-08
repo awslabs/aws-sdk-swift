b30e3dcd0844a6f074195262ccdadab697f16a74
Josh Elkins <jbelkins@amazon.com>
2025-09-08 14:46:23 -0500

    Cleanup

--- scripts/ci_steps/log_tool_versions.sh
+++ scripts/ci_steps/log_tool_versions.sh
@@ -46,15 +46,6 @@ else
 fi
 echo
 
-if command -v xcrun &> /dev/null
-then
-  which xcrun
-  xcrun simctl list
-else
-  echo "xcrun not installed"
-fi
-echo
-
 if command -v java &> /dev/null
 then
   which java
