8b1d755f045770e1b4b57f4643b2623e1ef01a7e
Josh Elkins <jbelkins@amazon.com>
2025-10-28 17:01:45 -0500

    Fix filename, Swiftlint

--- Sources/Core/AWSClientRuntime/Sources/AWSClientRuntime/AWSServiceClient.swift
+++ Sources/Core/AWSClientRuntime/Sources/AWSClientRuntime/AWSServiceClient.swift
@@ -11,7 +11,7 @@ import let AWSSDKDynamic.packageVersion
 public protocol AWSServiceClient: ClientRuntime.Client {}
 
 public extension AWSServiceClient {
-    
+
     /// The version identifier (i.e. `1.2.3`) for this AWS service client.
     static var version: String {
         AWSSDKDynamic.packageVersion
