9fbc675d6a3346ebc1a9f9be428a731130c057f5
Sichan Yoo <chanyoo@amazon.com>
2025-11-06 12:33:14 -0800

    Bump aws-crt-swift version to v0.54.2

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ import PackageDescription
 // MARK: - Dynamic Content
 
 let clientRuntimeVersion: Version = "0.168.0"
-let crtVersion: Version = "0.54.1"
+let crtVersion: Version = "0.54.2"
 
 let excludeRuntimeUnitTests = false
 
