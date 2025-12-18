e4b821b1c04cd8c4448eb87b8d06ca6031af0be0
Chan <55515281+sichanyoo@users.noreply.github.com>
2025-12-18 15:09:21 -0800

    chore: Remove glacier integration test (#2085)
    Co-authored-by: Sichan Yoo <chanyoo@amazon.com>

--- IntegrationTests/Package.swift
+++ IntegrationTests/Package.swift
@@ -52,7 +52,6 @@ private var integrationTestTargets: [Target] {
         "AWSEC2",
         "AWSECS",
         "AWSEventBridge",
-        "AWSGlacier",
         "AWSKinesis",
         "AWSMediaConvert",
         "AWSRoute53",
@@ -83,8 +82,6 @@ private func integrationTestTarget(_ name: String) -> Target {
             "README.md",
             "Resources/ECSIntegTestApp/"
         ]
-    case "AWSGlacier":
-        additionalDependencies = ["AWSSTS"]
     case "AWSS3":
         additionalDependencies = ["AWSSSOAdmin", "AWSS3Control", "AWSSTS"]
     case "AWSEventBridge":
