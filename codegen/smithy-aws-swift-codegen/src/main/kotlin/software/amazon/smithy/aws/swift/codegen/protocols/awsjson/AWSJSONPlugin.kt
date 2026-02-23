26e8c174e813d86b53ea40afc7ba05167792aa46
Josh Elkins <jbelkins@amazon.com>
2026-02-23 14:26:28 -0600

    Fix ktlint

--- codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/protocols/awsjson/AWSJSONPlugin.kt
+++ codegen/smithy-aws-swift-codegen/src/main/kotlin/software/amazon/smithy/aws/swift/codegen/protocols/awsjson/AWSJSONPlugin.kt
@@ -4,6 +4,6 @@ import software.amazon.smithy.aws.swift.codegen.swiftmodules.AWSSDKAWSJSONTypes
 import software.amazon.smithy.codegen.core.Symbol
 import software.amazon.smithy.swift.codegen.integration.Plugin
 
-class AWSJSONPlugin: Plugin {
+class AWSJSONPlugin : Plugin {
     override val className: Symbol = AWSSDKAWSJSONTypes.Plugin
 }
