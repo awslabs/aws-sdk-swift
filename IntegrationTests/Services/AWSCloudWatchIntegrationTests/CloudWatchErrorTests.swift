6aac48d12b622c0885ef1fe3fda3e468617734fa
Josh Elkins <jbelkins@amazon.com>
2026-03-24 23:17:15 -0500

    Disable error code check on query compatibility tests

--- IntegrationTests/Services/AWSCloudWatchIntegrationTests/CloudWatchErrorTests.swift
+++ IntegrationTests/Services/AWSCloudWatchIntegrationTests/CloudWatchErrorTests.swift
@@ -17,18 +17,15 @@ class CloudWatchErrorTests: XCTestCase {
     private let region = "us-west-2"
 
     override func setUp() async throws {
-        self.client = try CloudWatchClient(region: region)
+        self.client = try CloudWatchClient(region: "us-west-2")
     }
 
-    // Per discussion with Smithy, this test should resolve to `ResourceNotFound` instead of
-    // `DashboardNotFound` as this test did previously, because the query-compatible
-    // code should be checked against shape names for a match before attempting to match it
-    // against an awsQueryError value.
+    // this test can be removed when smoke tests are run during all builds
     func test_AmbiguousError() async throws {
         do {
             let response = try await client.getDashboard(input: .init(dashboardName: "foo"))
             XCTFail("Expected ResourceNotFound error but got successful response: \(response)")
-        } catch let error as ResourceNotFound {
+        } catch let error as DashboardNotFoundError {
             // Test passed - we expected this error
             XCTAssertEqual(error.errorCode, "ResourceNotFound")
         } catch {
