a535767f9b35e59a8af3f812e347f821ef9549ce
Josh Elkins <jbelkins@users.noreply.github.com>
2026-04-28 14:29:51 -0500

    chore: Quit the prepare_release CLI cmd early when build request or mapping JSON is missing (#2153)

--- AWSSDKSwiftCLI/Sources/AWSSDKSwiftCLI/Commands/AWSSDKSwiftCLI/Subcommands/PrepareRelease.swift
+++ AWSSDKSwiftCLI/Sources/AWSSDKSwiftCLI/Commands/AWSSDKSwiftCLI/Subcommands/PrepareRelease.swift
@@ -81,21 +81,8 @@ struct PrepareRelease {
     func run() throws {
         try FileManager.default.changeWorkingDirectory(repoPath)
 
-        let previousVersion = try getPreviousVersion()
-        guard try repoHasChanges(previousVersion) else {
-            // If repo has no changes, create an empty release-manifest.json file.
-            // Empty manifest file makes GitHubReleasePublisher be no-op.
-            // The manifest file is required regardless of whether there should
-            // be a release or not.
-            log("Repo has no changes to publish.")
-            log("Writing empty manifest and exiting.")
-            try createEmptyReleaseManifest()
-            // Return without creating new commit or tag in local repos.
-            // This makes GitPublisher be no-op.
-            return
-        }
-
         // Rename feature-service-id-smithy.json to feature-service-id.json if it exists
+        // This is needed when a build is triggered manually & merge step doesn't run
         if FileManager.default.fileExists(atPath: "../feature-service-id-smithy.json") {
             log("Renaming feature-service-id-smithy.json to feature-service-id.json.")
             try FileManager.default.moveItem(atPath: "../feature-service-id-smithy.json", toPath: "../feature-service-id.json")
@@ -106,6 +93,20 @@ struct PrepareRelease {
             // If the build request or mapping input files
             // don't exist, create an empty release-manifest.json file.
             log("build-request.json and/or feature-service-id.json don't exist.")
+            log("Writing empty manifest and exiting.")
+            try createEmptyReleaseManifest()
+            // Return without creating new commit or tag in local repos.
+            // This makes GitPublisher be no-op.
+            return
+        }
+
+        let previousVersion = try getPreviousVersion()
+        guard try repoHasChanges(previousVersion) else {
+            // If repo has no changes, create an empty release-manifest.json file.
+            // Empty manifest file makes GitHubReleasePublisher be no-op.
+            // The manifest file is required regardless of whether there should
+            // be a release or not.
+            log("Repo has no changes to publish.")
             log("Writing empty manifest and exiting.")
             try createEmptyReleaseManifest()
             // Return without creating new commit or tag in local repos.
