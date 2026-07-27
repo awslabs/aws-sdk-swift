af095520953c9da97eebf73096773a86b4e2e1bd
Josh Elkins <jbelkins@users.noreply.github.com>
2026-07-27 16:44:07 -0500

    chore: Update aws-crt-swift to 0.64.0 (#2204)

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ import PackageDescription
 // MARK: - Dynamic Content
 
 let clientRuntimeVersion: Version = "0.234.0"
-let crtVersion: Version = "0.63.0"
+let crtVersion: Version = "0.64.0"
 
 let excludeRuntimeUnitTests = false
 
